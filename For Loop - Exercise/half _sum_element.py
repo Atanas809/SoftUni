560a59a2053af86289f2e3d97e2f76697b9dd962
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-27 13:15:02 +0300

    Update half _sum_element.py

--- For Loop - Exercise/half _sum_element.py
+++ For Loop - Exercise/half _sum_element.py
@@ -1,21 +1,20 @@
-# Задача 2:
-# import sys
-#
-# count_numbers = int(input())
-#
-# max_number = -sys.maxsize
-# sum_numbers = 0
-#
-# for number in range(count_numbers):
-#     current_number = int(input())
-#     if current_number > max_number:
-#         max_number = current_number
-#     sum_numbers += current_number
-# if max_number == sum_numbers - max_number:
-#     print("Yes")
-#     print(f"Sum = {max_number}")
-# else:
-#     sum_others = sum_numbers - max_number
-#     difference = abs(max_number - sum_others)
-#     print("No")
-#     print(f"Diff = {difference}")
+import sys
+
+count_numbers = int(input())
+
+max_number = -sys.maxsize
+sum_numbers = 0
+
+for number in range(count_numbers):
+    current_number = int(input())
+    if current_number > max_number:
+        max_number = current_number
+    sum_numbers += current_number
+if max_number == sum_numbers - max_number:
+    print("Yes")
+    print(f"Sum = {max_number}")
+else:
+    sum_others = sum_numbers - max_number
+    difference = abs(max_number - sum_others)
+    print("No")
+    print(f"Diff = {difference}")

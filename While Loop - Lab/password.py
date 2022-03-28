8e946fe62e710eb1f4de35e47a5de4abfe9f2b76
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-28 12:55:48 +0300

    Update password.py

--- While Loop - Lab/password.py
+++ While Loop - Lab/password.py
@@ -1,9 +1,8 @@
-# Задача 2:
-# username = input()
-# password = input()
-#
-# entered_password = input()
-#
-# while entered_password != password:
-#     entered_password = input()
-# print(f"Welcome {username}!")
+username = input()
+password = input()
+
+entered_password = input()
+
+while entered_password != password:
+    entered_password = input()
+print(f"Welcome {username}!")

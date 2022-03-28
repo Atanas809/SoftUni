3b2e219e819c8e494f5b0f92580d652bd8c75dab
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-28 12:55:19 +0300

    Update max_number.py

--- While Loop - Lab/max_number.py
+++ While Loop - Lab/max_number.py
@@ -1,13 +1,12 @@
-# Задача 6:
-# import sys
-#
-# max_number = - sys.maxsize
-#
-# command = input()
-#
-# while command != "Stop":
-#     current_number = int(command)
-#     if current_number > max_number:
-#         max_number = current_number
-#     command = input()
-# print(max_number)
+import sys
+
+max_number = - sys.maxsize
+
+command = input()
+
+while command != "Stop":
+    current_number = int(command)
+    if current_number > max_number:
+        max_number = current_number
+    command = input()
+print(max_number)

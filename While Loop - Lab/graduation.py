a1ccd0338f69c75ff751c3b2f06955b204b7ea5e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-28 12:55:00 +0300

    Update graduation.py

--- While Loop - Lab/graduation.py
+++ While Loop - Lab/graduation.py
@@ -1,21 +1,21 @@
 student_name = input()
-# average_grade = 0
-# current_class = 1
-# bad_grades = 0
-# is_ejected = False
-#
-# while current_class <= 12:
-#     current_grade = float(input())
-#     if current_grade < 4:
-#         bad_grades += 1
-#         if bad_grades == 2:
-#             is_ejected = True
-#             break
-#         continue
-#     average_grade += current_grade
-#     current_class += 1
-# if is_ejected:
-#     print(f"{student_name} has been excluded at {current_class} grade")
-# else:
-#     average_grade /= 12
-#     print(f"{student_name} graduated. Average grade: {average_grade:.2f}")
+average_grade = 0
+current_class = 1
+bad_grades = 0
+is_ejected = False
+
+while current_class <= 12:
+    current_grade = float(input())
+    if current_grade < 4:
+        bad_grades += 1
+        if bad_grades == 2:
+            is_ejected = True
+            break
+        continue
+    average_grade += current_grade
+    current_class += 1
+if is_ejected:
+    print(f"{student_name} has been excluded at {current_class} grade")
+else:
+    average_grade /= 12
+    print(f"{student_name} graduated. Average grade: {average_grade:.2f}")

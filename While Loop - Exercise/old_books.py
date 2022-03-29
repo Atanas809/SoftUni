b37025f2432e8d7d6c3dd06a5add2f6fcebfe459
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-29 23:56:14 +0300

    Update old_books.py

--- While Loop - Exercise/old_books.py
+++ While Loop - Exercise/old_books.py
@@ -1,18 +1,18 @@
 # Задача 1:
-# needed_book = input()
-# checked_books = 0
-# is_finded = False
-#
-# current_book = input()
-# while current_book != "No More Books":
-#     if needed_book != current_book:
-#         checked_books += 1
-#         current_book = input()
-#     else:
-#         is_finded = True
-#         break
-# if is_finded:
-#     print(f"You checked {checked_books} books and found it.")
-# else:
-#     print("The book you search is not here!")
-#     print(f"You checked {checked_books} books.")
+needed_book = input()
+checked_books = 0
+is_finded = False
+
+current_book = input()
+while current_book != "No More Books":
+    if needed_book != current_book:
+        checked_books += 1
+        current_book = input()
+    else:
+        is_finded = True
+        break
+if is_finded:
+    print(f"You checked {checked_books} books and found it.")
+else:
+    print("The book you search is not here!")
+    print(f"You checked {checked_books} books.")

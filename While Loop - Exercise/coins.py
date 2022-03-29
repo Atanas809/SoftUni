57f683d30787c329819a9d329efb02c024521d7f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-29 23:54:52 +0300

    Update coins.py

--- While Loop - Exercise/coins.py
+++ While Loop - Exercise/coins.py
@@ -1,40 +1,40 @@
 # Задача 5:
-# coins = float(input())
-# coins = int(coins * 100)
-# coins_counter = 0
-#
-# while coins > 0:
-#     if coins - 200 >= 0:
-#         coins_counter += 1
-#         coins -= 200
-#         continue
-#     if coins - 100 >= 0:
-#         coins_counter += 1
-#         coins -= 100
-#         continue
-#     if coins - 50 >= 0:
-#         coins_counter += 1
-#         coins -= 50
-#         continue
-#     if coins - 20 >= 0:
-#         coins_counter += 1
-#         coins -= 20
-#         continue
-#     if coins - 10 >= 0:
-#         coins_counter += 1
-#         coins -= 10
-#         continue
-#     if coins - 5 >= 0:
-#         coins_counter += 1
-#         coins -= 5
-#         continue
-#     if coins - 2 >= 0:
-#         coins_counter += 1
-#         coins -= 2
-#         continue
-#     if coins - 1 >= 0:
-#         coins_counter += 1
-#         coins -= 1
-#         continue
-#     break
-# print(coins_counter)
+coins = float(input())
+coins = int(coins * 100)
+coins_counter = 0
+
+while coins > 0:
+    if coins - 200 >= 0:
+        coins_counter += 1
+        coins -= 200
+        continue
+    if coins - 100 >= 0:
+        coins_counter += 1
+        coins -= 100
+        continue
+    if coins - 50 >= 0:
+        coins_counter += 1
+        coins -= 50
+        continue
+    if coins - 20 >= 0:
+        coins_counter += 1
+        coins -= 20
+        continue
+    if coins - 10 >= 0:
+        coins_counter += 1
+        coins -= 10
+        continue
+    if coins - 5 >= 0:
+        coins_counter += 1
+        coins -= 5
+        continue
+    if coins - 2 >= 0:
+        coins_counter += 1
+        coins -= 2
+        continue
+    if coins - 1 >= 0:
+        coins_counter += 1
+        coins -= 1
+        continue
+    break
+print(coins_counter)

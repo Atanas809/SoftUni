1d30844a8db2200e661ba87ecf4c6f7b0ae43540
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-29 23:54:17 +0300

    Update cake.py

--- While Loop - Exercise/cake.py
+++ While Loop - Exercise/cake.py
@@ -1,21 +1,21 @@
 # Задача 6:
-# width = int(input())
-# length = int(input())
-# is_done = False
-# total_volume = width * length
-# number_of_pieces = 0
-#
-# command = input()
-# while command != "STOP":
-#     current_pieces = int(command)
-#     number_of_pieces += current_pieces
-#     if number_of_pieces > total_volume:
-#         is_done = True
-#         break
-#     command = input()
-# if is_done:
-#     difference = abs(total_volume - number_of_pieces)
-#     print(f"No more cake left! You need {difference} pieces more.")
-# else:
-#     difference = total_volume - number_of_pieces
-#     print(f"{difference} pieces are left.")
+width = int(input())
+length = int(input())
+is_done = False
+total_volume = width * length
+number_of_pieces = 0
+
+command = input()
+while command != "STOP":
+    current_pieces = int(command)
+    number_of_pieces += current_pieces
+    if number_of_pieces > total_volume:
+        is_done = True
+        break
+    command = input()
+if is_done:
+    difference = abs(total_volume - number_of_pieces)
+    print(f"No more cake left! You need {difference} pieces more.")
+else:
+    difference = total_volume - number_of_pieces
+    print(f"{difference} pieces are left.")

c14d4c6879eedad78b115c000c78d5c1e51cde93
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-30 13:14:44 +0300

    Update clock.py

--- Nested Loops - Lab/clock.py
+++ Nested Loops - Lab/clock.py
@@ -1,7 +1,7 @@
  for hour in range(0, 24):
-#     for minutes in range(0, 60):
-#         print(f"{hour}:{minutes}")
-        # if minutes < 10:
-        #     print(f"{hour}:0{minutes}")
-        # else:
-        #     print(f"{hour}:{minutes}")
+    for minutes in range(0, 60):
+        print(f"{hour}:{minutes}")
+        if minutes < 10:
+            print(f"{hour}:0{minutes}")
+        else:
+            print(f"{hour}:{minutes}")

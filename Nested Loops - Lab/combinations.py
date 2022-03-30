f55848392fecb2e899c30c6a593120d52a371595
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-30 13:14:59 +0300

    Update combinations.py

--- Nested Loops - Lab/combinations.py
+++ Nested Loops - Lab/combinations.py
@@ -1,10 +1,10 @@
 # Задача 3:
-# number = int(input())
-# total_combinations = 0
-#
-# for x1 in range(0, number + 1):
-#     for x2 in range(0, number + 1):
-#         for x3 in range(0, number + 1):
-#             if x1 + x2 + x3 == number:
-#                 total_combinations += 1
-# print(total_combinations)
+number = int(input())
+total_combinations = 0
+
+for x1 in range(0, number + 1):
+    for x2 in range(0, number + 1):
+        for x3 in range(0, number + 1):
+            if x1 + x2 + x3 == number:
+                total_combinations += 1
+print(total_combinations)

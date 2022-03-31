5c1b5a6659b6e43f062d713f0ff8021a03db4b17
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-31 13:13:38 +0300

    Update number_pyramid.py

--- Nested Loops - Exercise/number_pyramid.py
+++ Nested Loops - Exercise/number_pyramid.py
@@ -1,20 +1,20 @@
 # Задача 1:
-# number = int(input())
-# counter = 1
-# all_numbers_printed = False
-#
-# for rows in range(1, number + 1):
-#     for cols in range(1, rows + 1):
-#         if counter == 1 and counter <= number:
-#             print(counter, end = " ")
-#             counter += 1
-#         elif counter != 1 and counter <= number:
-#             print(counter, end = " ")
-#             counter += 1
-#         elif counter == number:
-#             all_numbers_printed = True
-#             break
-#     if all_numbers_printed:
-#         break
-#     print()
+number = int(input())
+counter = 1
+all_numbers_printed = False
+
+for rows in range(1, number + 1):
+    for cols in range(1, rows + 1):
+        if counter == 1 and counter <= number:
+            print(counter, end = " ")
+            counter += 1
+        elif counter != 1 and counter <= number:
+            print(counter, end = " ")
+            counter += 1
+        elif counter == number:
+            all_numbers_printed = True
+            break
+    if all_numbers_printed:
+        break
+    print()
 

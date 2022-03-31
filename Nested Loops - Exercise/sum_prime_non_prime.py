93b0d71c44c739c5f2c658c945200b37d97740ec
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-03-31 13:14:20 +0300

    Update sum_prime_non_prime.py

--- Nested Loops - Exercise/sum_prime_non_prime.py
+++ Nested Loops - Exercise/sum_prime_non_prime.py
@@ -1,22 +1,22 @@
-# Задача 3:
-# command = input()
-# prime_numbers_sum = 0
-# non_prime_numbers_sum = 0
-# while command != "stop":
-#     current_number = int(command)
-#     if current_number < 0:
-#         print("Number is negative.")
-#     else:
-#         number_is_prime = True
-#         for number in range(2, current_number):
-#             if current_number % number == 0:
-#                 number_is_prime = False
-#                 break
-#         if number_is_prime:
-#             prime_numbers_sum += current_number
-#         else:
-#             non_prime_numbers_sum += current_number
-#
-#     command = input()
-# print(f"Sum of all prime numbers is: {prime_numbers_sum}")
-# print(f"Sum of all non prime numbers is: {non_prime_numbers_sum}")
+Задача 3:
+command = input()
+prime_numbers_sum = 0
+non_prime_numbers_sum = 0
+while command != "stop":
+    current_number = int(command)
+    if current_number < 0:
+        print("Number is negative.")
+    else:
+        number_is_prime = True
+        for number in range(2, current_number):
+            if current_number % number == 0:
+                number_is_prime = False
+                break
+        if number_is_prime:
+            prime_numbers_sum += current_number
+        else:
+            non_prime_numbers_sum += current_number
+
+    command = input()
+print(f"Sum of all prime numbers is: {prime_numbers_sum}")
+print(f"Sum of all non prime numbers is: {non_prime_numbers_sum}")

88ac6faa61afc4730680b637115e7de570d979db
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-01 11:11:11 +0300

    Update area_of_figures.py

--- Python Tasks/area_of_figures.py
+++ Python Tasks/area_of_figures.py
@@ -1,17 +1,16 @@
-# Задача 7:
 from math import pi
 
-type_of_figure = input()
-a = float(input())
+# type_of_figure = input()
+# a = float(input())
 
-if type_of_figure == "square":
-    area = a * a
-elif type_of_figure == "rectangle":
-    b = float(input())
-    area = a * b
-elif type_of_figure == "circle":
-    area = pi * (a ** 2)
-else:
-    b = float(input())
-    area = (a * b)/ 2
-print(f"{area:.3f}")
+# if type_of_figure == "square":
+#     area = a * a
+# elif type_of_figure == "rectangle":
+#     b = float(input())
+#     area = a * b
+# elif type_of_figure == "circle":
+#     area = pi * (a ** 2)
+# else:
+#     b = float(input())
+#     area = (a * b)/ 2
+# print(f"{area:.3f}")

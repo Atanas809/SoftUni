52208fdc3fba0c0e6ae313a90c2ecdfbb955cbd9
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-02 10:46:49 +0300

    Update pet_shop.py

--- Exercises/pet_shop.py
+++ Exercises/pet_shop.py
@@ -1,10 +1,9 @@
-# задача 8:
 food_for_dogs = int(input())
-food_for_cats = int(input())
+# food_for_cats = int(input())
 
-price_food_for_dogs = food_for_dogs * 2.50
-price_food_for_cats = food_for_cats * 4
+# price_food_for_dogs = food_for_dogs * 2.50
+# price_food_for_cats = food_for_cats * 4
 
-total_price = price_food_for_dogs + price_food_for_cats
+# total_price = price_food_for_dogs + price_food_for_cats
 
-print(f"{total_price} lv.")
+# print(f"{total_price} lv.")

eb3e42809d90dd34fd647c760c3ebbd6f770a086
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-02 10:57:16 +0300

    Update speed_info.py

--- Python Tasks/speed_info.py
+++ Python Tasks/speed_info.py
@@ -1,12 +1,12 @@
 speed = float(input())
 
 if speed <= 10:
-    print("slow")
+    print("too slow")
 elif speed <= 50:
     print("average")
 elif speed <= 150:
-    print("fast")
+    print("too fast")
 elif speed <= 1000:
     print("ultra fast")
 else:
-    print("extremely fast")
+    print("extremely fast, please slow down")

6360f896d4826ddfba13df6fedb738c886d3a873
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-04 12:05:07 +0300

    Update number_100....200.py

--- Exercises/number_100....200.py
+++ Exercises/number_100....200.py
@@ -1,9 +1,9 @@
 # Задача 5:
-number = int(input())
+# number = int(input())
 
-if number < 100:
-    print("Less than 100")
-elif number <= 200:
-    print("Between 100 and 200")
-else:
-    print("Greater than 200")
+# if number < 100:
+#     print("Less than 100")
+# elif number <= 200:
+#     print("Between 100 and 200")
+# else:
+#     print("Greater than 200")

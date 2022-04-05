f118340ab7a206c6fec079629ab54334d2bdfec3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-05 10:14:36 +0300

    Update greater_number.py

--- Exercises/greater_number.py
+++ Exercises/greater_number.py
@@ -4,4 +4,4 @@ second_number = int(input())
 if first_number > second_number:
     print(first_number)
 else:
-    print(second_number)
+    print(second_number) # because first_number == second_number, can also print first_number 

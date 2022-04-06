ad52758d70aa1e4e304f00815bfe811e33a31c6a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:15:28 +0300

    Update deer of_santa.py

--- Python Basics/Exam Preparation - Python Basics/deer of_santa.py
+++ Python Basics/Exam Preparation - Python Basics/deer of_santa.py
@@ -1,24 +1,24 @@
 # import math
-#
-# number_of_absent = int(input())
-# food_in_kilos = int(input())
-# food_for_first_deer = float(input())
-# food_for_second_deer = float(input())
-# food_for_third_deer = float(input())
-#
-#
-# needed_food_for_first_deer = food_for_first_deer * number_of_absent
-# needed_food_for_second_deer = food_for_second_deer * number_of_absent
-# needed_food_for_third_deer = food_for_third_deer * number_of_absent
-#
-# needed_food = needed_food_for_first_deer + needed_food_for_second_deer + needed_food_for_third_deer
-#
-#
-# if needed_food <= food_in_kilos:
-#     difference = math.floor(abs(needed_food - food_in_kilos))
-#     print(f"{difference} kilos of food left.")
-# else:
-#     difference = math.ceil(abs(food_in_kilos - needed_food))
-#     print(f"{difference} more kilos of food are needed.")
+
+number_of_absent = int(input())
+food_in_kilos = int(input())
+food_for_first_deer = float(input())
+food_for_second_deer = float(input())
+food_for_third_deer = float(input())
+
+
+needed_food_for_first_deer = food_for_first_deer * number_of_absent
+needed_food_for_second_deer = food_for_second_deer * number_of_absent
+needed_food_for_third_deer = food_for_third_deer * number_of_absent
+
+needed_food = needed_food_for_first_deer + needed_food_for_second_deer + needed_food_for_third_deer
+
+
+if needed_food <= food_in_kilos:
+    difference = math.floor(abs(needed_food - food_in_kilos))
+    print(f"{difference} kilos of food left.")
+else:
+    difference = math.ceil(abs(food_in_kilos - needed_food))
+    print(f"{difference} more kilos of food are needed.")
 
 

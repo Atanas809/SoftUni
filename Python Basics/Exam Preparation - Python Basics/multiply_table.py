af643e07aceb0332a6802eb92a6c49f8ee36c3d3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:17:42 +0300

    Update multiply_table.py

--- Python Basics/Exam Preparation - Python Basics/multiply_table.py
+++ Python Basics/Exam Preparation - Python Basics/multiply_table.py
@@ -1,11 +1,11 @@
-# number = int(input())
-#
-# first_number = number % 10
-# second_number = (number // 10) % 10
-# third_number = (number // 10) // 10
-#
-# for x1 in range(1, first_number + 1):
-#     for x2 in range(1, second_number + 1):
-#         for x3 in range(1, third_number + 1):
-#             result = x1 * x2 * x3
-#             print(f"{x1} * {x2} * {x3} = {result}")
+number = int(input())
+
+first_number = number % 10
+second_number = (number // 10) % 10
+third_number = (number // 10) // 10
+
+for x1 in range(1, first_number + 1):
+    for x2 in range(1, second_number + 1):
+        for x3 in range(1, third_number + 1):
+            result = x1 * x2 * x3
+            print(f"{x1} * {x2} * {x3} = {result}")

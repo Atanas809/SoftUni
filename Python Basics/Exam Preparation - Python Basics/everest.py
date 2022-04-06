6639be1dccf462b5275547e729a28ec1f39c03a8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:17:19 +0300

    Update everest.py

--- Python Basics/Exam Preparation - Python Basics/everest.py
+++ Python Basics/Exam Preparation - Python Basics/everest.py
@@ -1,24 +1,27 @@
-current_metres = 5364
+current_meters = 5364
 goal = 8848
 current_day = 1
 command = input()
 goal_is_reached = False
+
 while command != "END":
     will_sleep = command
-    more_metres = int(input())
+    more_meters = int(input())
     if current_day >= 5:
         break
     if will_sleep == "Yes":
         current_day += 1
-        current_metres += more_metres
+        current_meters += more_meters
     elif will_sleep == "No":
-        current_metres += more_metres
-    if goal <= current_metres:
+        current_meters += more_meters
+    if goal <= current_meters:
         goal_is_reached = True
         break
     command = input()
+    
 if goal_is_reached:
     print(f"Goal reached for {current_day} days!")
+    
 else:
     print("Failed!")
-    print(f"{current_metres}")
+    print(f"{current_meters}")

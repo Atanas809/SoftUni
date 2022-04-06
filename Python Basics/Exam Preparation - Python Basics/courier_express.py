262951aec62619e3f653285d5d3bdd355ebfa3e7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:15:11 +0300

    Update courier_express.py

--- Python Basics/Exam Preparation - Python Basics/courier_express.py
+++ Python Basics/Exam Preparation - Python Basics/courier_express.py
@@ -1,6 +1,7 @@
-# weight_in_kilos = float(input())
-# type_of_service = input()
-# distance_in_kilometres = int(input())
+weight_in_kilos = float(input())
+type_of_service = input()
+distance_in_kilometres = int(input())
+
 if type_of_service == "standard":
     if weight_in_kilos < 1:
         price = 0.03 * distance_in_kilometres
@@ -12,6 +13,7 @@ if type_of_service == "standard":
         price = 0.15 * distance_in_kilometres
     elif weight_in_kilos < 150:
         price = 0.20 * distance_in_kilometres
+        
 if type_of_service == "express":
     if weight_in_kilos < 1:
         price = (((0.8 * 0.03) * weight_in_kilos) * distance_in_kilometres) + distance_in_kilometres * 0.03
@@ -23,4 +25,5 @@ if type_of_service == "express":
         price = (((0.02 * 0.15) * weight_in_kilos) * distance_in_kilometres) + distance_in_kilometres * 0.15
     elif weight_in_kilos < 150:
         price = (((0.01 * 0.20) * weight_in_kilos) * distance_in_kilometres) + distance_in_kilometres * 0.20
+        
 print(f"The delivery of your shipment with weight of {weight_in_kilos:.3f} kg. would cost {price:.2f} lv.")

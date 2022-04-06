0ba92ed9531e91cbd1f1bc8336fe78ca99cbd9cf
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:17:55 +0300

    Update pc_store.py

--- Python Basics/Exam Preparation - Python Basics/pc_store.py
+++ Python Basics/Exam Preparation - Python Basics/pc_store.py
@@ -2,16 +2,16 @@
 # video_card_in_dollars = float(input())
 # ram_in_dollars = float(input())
 # number_of_ram = int(input())
-# discount = float(input())
-#
-# processor_in_leva = processor_in_dollars * 1.57
-# video_card_in_leva = video_card_in_dollars * 1.57
-# ram_in_leva = (ram_in_dollars * 1.57) * number_of_ram
-#
-# processor_discount = processor_in_leva * discount
-# video_card_discount = video_card_in_leva * discount
-#
-# total_leva = (processor_in_leva - processor_discount) + (video_card_in_leva - video_card_discount) + ram_in_leva
-#
-# print(f"Money needed - {total_leva:.2f} leva.")
-#
+discount = float(input())
+
+processor_in_leva = processor_in_dollars * 1.57
+video_card_in_leva = video_card_in_dollars * 1.57
+ram_in_leva = (ram_in_dollars * 1.57) * number_of_ram
+
+processor_discount = processor_in_leva * discount
+video_card_discount = video_card_in_leva * discount
+
+total_leva = (processor_in_leva - processor_discount) + (video_card_in_leva - video_card_discount) + ram_in_leva
+
+print(f"Money needed - {total_leva:.2f} leva.")
+

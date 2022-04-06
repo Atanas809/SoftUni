141f352a061a7a7a313cb0144254595183138e16
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-06 11:15:42 +0300

    Update deer of_santa.py

--- Python Basics/Exam Preparation - Python Basics/deer of_santa.py
+++ Python Basics/Exam Preparation - Python Basics/deer of_santa.py
@@ -1,4 +1,4 @@
-# import math
+import math
 
 number_of_absent = int(input())
 food_in_kilos = int(input())
@@ -17,8 +17,7 @@ needed_food = needed_food_for_first_deer + needed_food_for_second_deer + needed_
 if needed_food <= food_in_kilos:
     difference = math.floor(abs(needed_food - food_in_kilos))
     print(f"{difference} kilos of food left.")
+    
 else:
     difference = math.ceil(abs(food_in_kilos - needed_food))
     print(f"{difference} more kilos of food are needed.")
-
-

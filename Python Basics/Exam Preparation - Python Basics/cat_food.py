28ab77d6ef074b3b6087a85638f35df8b8be148b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-07 11:17:42 +0300

    Update cat_food.py

--- Python Basics/Exam Preparation - Python Basics/cat_food.py
+++ Python Basics/Exam Preparation - Python Basics/cat_food.py
@@ -1,11 +1,11 @@
-# number_of_cats = int(input())
+number_of_cats = int(input())
 
-# small_cats = 0
-# big_cats = 0
-# huge_cats = 0
-# needed_food = 0
+small_cats = 0
+big_cats = 0
+huge_cats = 0
+needed_food = 0
 
-for cat in range(1, number_of_cats + 1):
+for cat in range(number_of_cats):
     food_in_grams = float(input())
     needed_food += food_in_grams
     if 100 <= food_in_grams < 200:

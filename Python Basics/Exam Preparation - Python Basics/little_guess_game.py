a0c77d2e73fd73d45efa7ee20b4d4b29ac5be359
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-07 11:21:30 +0300

    Update and rename main.py to little_guess_game.py

--- Python Basics/Exam Preparation - Python Basics/little_guess_game.py
+++ Python Basics/Exam Preparation - Python Basics/little_guess_game.py
@@ -6,4 +6,4 @@ while number != correct_number:
     print("Try another one:", end = " ")
     number = int(input())
 if number == correct_number:
-    print("Congratulation Borislava you win your bf's penis!")
+    print("You won!")

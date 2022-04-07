b232f9018c5a8eb996f25df4b6289d5a0ec2cd30
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-07 11:19:11 +0300

    Update football_souvenirs.py

--- Python Basics/Exam Preparation - Python Basics/football_souvenirs.py
+++ Python Basics/Exam Preparation - Python Basics/football_souvenirs.py
@@ -30,18 +30,18 @@ if team == "Brazil":
         price += number_of_souvenirs * 1.20
     else:
         souvenirs_is_valid = False
-if team == "Croatia":
-    team_is_valid = True
-    if souvenirs == "flags":
-        price += number_of_souvenirs * 2.75
-    elif souvenirs == "caps":
-        price += number_of_souvenirs * 6.90
-    elif souvenirs == "posters":
-        price += number_of_souvenirs * 4.95
-    elif souvenirs == "stickers":
-        price += number_of_souvenirs * 1.10
-    else:
-        souvenirs_is_valid = False
+# if team == "Croatia":
+#     team_is_valid = True
+#     if souvenirs == "flags":
+#         price += number_of_souvenirs * 2.75
+#     elif souvenirs == "caps":
+#         price += number_of_souvenirs * 6.90
+#     elif souvenirs == "posters":
+#         price += number_of_souvenirs * 4.95
+#     elif souvenirs == "stickers":
+#         price += number_of_souvenirs * 1.10
+#     else:
+#         souvenirs_is_valid = False
 if team == "Denmark":
     team_is_valid = True
     if souvenirs == "flags":

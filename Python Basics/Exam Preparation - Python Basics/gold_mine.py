a6a1670e58f6d8b39b86af42bf23f480d6081a93
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-07 11:19:37 +0300

    Update gold_mine.py

--- Python Basics/Exam Preparation - Python Basics/gold_mine.py
+++ Python Basics/Exam Preparation - Python Basics/gold_mine.py
@@ -1,6 +1,6 @@
 number_of_locations = int(input())
 
-for location in range(1, number_of_locations + 1):
+for location in range(1, number_of_locations):
     total_average_yield = 0
     final_average_yield = 0
     average_yield = float(input())

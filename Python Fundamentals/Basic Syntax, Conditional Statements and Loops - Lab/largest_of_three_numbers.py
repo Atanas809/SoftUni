061342cf68cb9da2a26962950ba80ae9c5441f6b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-08 12:41:50 +0300

    Update largest_of_three_numbers.py

--- Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/largest_of_three_numbers.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/largest_of_three_numbers.py
@@ -1,17 +1,16 @@
-# Задача 1:
-# import sys
+import sys
 
-# first_number = int(input())
-# second_number = int(input())
-# third_number = int(input())
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
 
-# max_number = -sys.maxsize
+max_number = -sys.maxsize
 
-# if first_number > max_number:
-#     max_number = first_number
-# if second_number > max_number:
-#     max_number = second_number
-# if third_number > max_number:
-#     max_number = third_number
+if first_number > max_number:
+    max_number = first_number
+if second_number > max_number:
+    max_number = second_number
+if third_number > max_number:
+    max_number = third_number
 
-# print(max_number)
+print(max_number)

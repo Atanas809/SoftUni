194c26e89be641ef1366c0d877d173d417285506
Atanas809 <atanas2380@abv.bg>
2022-04-08 12:42:41 +0300

    Added ne files

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/number_definer.py
@@ -0,0 +1,19 @@
+# Задача 2:
+number = float(input())
+
+if number == 0:
+    print("zero")
+elif number > 0:
+    if 0 < number < 1:
+        print("small positive")
+    elif number > 1000000:
+        print("large positive")
+    else:
+        print("positive")
+else:
+    if 0 > number > -1:
+        print("small negative")
+    elif number < -1000000:
+        print("large negative")
+    else:
+        print("negative")
---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/word_reverse.py
@@ -0,0 +1,6 @@
+# Задача 3:
+word = input()
+reversed_word = ""
+for char in range(len(word) - 1, -1, -1):
+    reversed_word += word[char]
+print(reversed_word)

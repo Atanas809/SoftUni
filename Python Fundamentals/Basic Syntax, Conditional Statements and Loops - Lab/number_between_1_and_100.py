ecc8f4e5d867b7aa2e9b3ea16945867121559d77
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-08 12:46:17 +0300

    Update number_between_1_and_100.py

--- Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/number_between_1_and_100.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/number_between_1_and_100.py
@@ -1,5 +1,5 @@
-number = float(input())
+number = int(input())
 
 while number < 1 or number > 100:
-    number = float(input())
+    number = int(input())
 print(f"The number {number} is between 1 and 100")

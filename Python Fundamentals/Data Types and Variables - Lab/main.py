124ad280b3d50550b834f59feceb2193969a900b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-10 10:24:10 +0300

    Update main.py

--- Python Fundamentals/Data Types and Variables - Lab/main.py
+++ Python Fundamentals/Data Types and Variables - Lab/main.py
@@ -2,12 +2,12 @@
 
 import webbrowser as wb
 
-def web_automation():
-    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-    URLS = ("softuni.bg", "judge.softuni.org", "youtube.com")
+# def web_automation():
+#     chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+#     URLS = ("softuni.bg", "judge.softuni.org", "youtube.com")
 
-    for url in URLS:
-        print(f"Opening: {url}")
-        wb.get(chrome_path).open(url)
+#     for url in URLS:
+#         print(f"Opening: {url}")
+#         wb.get(chrome_path).open(url)
 
-web_automation()
+# web_automation()

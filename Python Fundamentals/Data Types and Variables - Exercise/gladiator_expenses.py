70ec08cf5ce45b59a1128d5fb4e7965ddd8f632c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-11 11:22:10 +0300

    Update gladiator_expenses.py

--- Python Fundamentals/Data Types and Variables - Exercise/gladiator_expenses.py
+++ Python Fundamentals/Data Types and Variables - Exercise/gladiator_expenses.py
@@ -18,4 +18,4 @@ for lost in range(1, lost_fights_count + 1):
             if broken_shield % 2 == 0:
                 total_price += armor_price
 
-print(f"Gladiator expenses: {price:.2f} aureus")
+print(f"Gladiator expenses: {total_price:.2f} aureus")

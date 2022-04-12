6aa5c01791546d5ccc894a4b96b026cffd0b625c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-12 15:31:45 +0300

    Update triples_of_latin_letters.py

--- Python Fundamentals/Data Types and Variables - Exercise/triples_of_latin_letters.py
+++ Python Fundamentals/Data Types and Variables - Exercise/triples_of_latin_letters.py
@@ -1,7 +1,6 @@
-# Задача 6:
 n = int(input())
 
-for x in range(n):
+for a in range(n):
     for y in range(n):
         for z in range(n):
             print(f"{chr(x + 97)}{chr(y + 97)}{chr(z + 97)}")

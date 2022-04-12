d4b40fa9795f05cd6ea0c034539f6a6354312ad5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-12 15:23:37 +0300

    Update party_profit.py

--- Python Fundamentals/Data Types and Variables - Exercise/party_profit.py
+++ Python Fundamentals/Data Types and Variables - Exercise/party_profit.py
@@ -4,16 +4,22 @@ days = int(input())
 coins = 0
 
 for day in range(1, days + 1):
+    
     if day % 10 == 0:
         group_size -= 2
+        
     if day % 15 == 0:
         group_size += 5
+        
     coins += 50
     coins -= group_size * 2
+    
     if day % 3 == 0:
         coins -= group_size * 3
+        
     if day % 5 == 0:
         coins += group_size * 20
+        
         if day % 3 == 0:
             coins -= group_size * 2
 

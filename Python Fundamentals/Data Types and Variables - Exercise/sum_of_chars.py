5782ec13f1435e7a125bab088266c7d11c246ae6
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-12 15:31:15 +0300

    Update sum_of_chars.py

--- Python Fundamentals/Data Types and Variables - Exercise/sum_of_chars.py
+++ Python Fundamentals/Data Types and Variables - Exercise/sum_of_chars.py
@@ -1,10 +1,9 @@
-# Задача 4:
 n = int(input())
 
-total_sum = 0
+final_sum = 0
 
 for number in range(1, n + 1):
     letter = input()
-    total_sum += ord(letter)
+    final_sum += ord(letter)
 
-print(f"The sum equals: {total_sum}")
+print(f"The sum equals: {final_sum}")

69a52a404da52e4d6c167b98e3e7804fc9778606
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-13 13:55:34 +0300

    Update list_statistics.py

--- Python Fundamentals/List - Basics/list_statistics.py
+++ Python Fundamentals/List - Basics/list_statistics.py
@@ -9,10 +9,12 @@ for _ in range(number):
 
     if num >= 0:
         positive_list.append(num)
+        
     else:
         negative_list.append(num)
 
 print(positive_list)
 print(negative_list)
+
 print(f"Count of positives: {len(positive_list)}")
 print(f"Sum of negatives: {sum(negative_list)}")

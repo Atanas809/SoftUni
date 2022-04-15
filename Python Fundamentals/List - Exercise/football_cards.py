0431539f7c61ee922bf23b5dc493589b19911126
Atanas809 <atanas2380@abv.bg>
2022-04-15 13:29:20 +0300

    Added new file

---
+++ Python Fundamentals/List - Exercise/football_cards.py
@@ -0,0 +1,45 @@
+# Задача 3:
+
+red_cards = input().split()
+
+a_team = 11
+b_team = 11
+
+is_stopped = False
+
+sent_off_players = list()
+
+for x in red_cards:
+
+    if is_stopped:
+        break
+
+    else:
+        info = x.split("-")
+
+        team = info[0]
+        player = info[1]
+
+        if team == "A":
+            if a_team < 7:
+                is_stopped = True
+                break
+            else:
+                if x not in sent_off_players:
+                    sent_off_players.append(x)
+                    a_team -= 1
+
+        elif team == "B":
+            if b_team < 7:
+                is_stopped = True
+                break
+            else:
+                if x not in sent_off_players:
+                    sent_off_players.append(x)
+                    b_team -= 1
+
+
+print(f"Team A - {a_team}; Team B - {b_team}")
+
+if is_stopped:
+    print("Game was terminated")

a27f23a9430d1eac69b696ff9bfa776ee5265805
Atanas809 <atanas2380@abv.bg>
2022-04-15 13:30:38 +0300

    Added new file

---
+++ Python Fundamentals/List - Exercise/invert_values.py
@@ -0,0 +1,17 @@
+# Задача 1:
+my_list = list(map(int, input().split()))
+
+new_list = list()
+
+for x in my_list:
+
+    if x <= 0:
+        x = abs(x)
+        new_list.append(x)
+
+    else:
+        x = f"-{x}"
+        new_list.append(int(x))
+
+
+print(new_list)

e083495af9333c8f6887acca9e9f79d0f1de74fa
Atanas809 <atanas2380@abv.bg>
2022-04-15 13:28:50 +0300

    Added new file

---
+++ Python Fundamentals/List - Exercise/faro_shuffle.py
@@ -0,0 +1,18 @@
+# Задача 5:
+cards = input().split()
+
+shuffles = int(input())
+
+length = len(cards)
+mid = length // 2
+
+for x in range(shuffles):
+    my_list = []
+    for y in range(0, mid):
+        my_list.append(cards[y])
+        my_list.append(cards[mid])
+        mid += 1
+    cards = my_list
+    mid = length // 2
+
+print(cards)

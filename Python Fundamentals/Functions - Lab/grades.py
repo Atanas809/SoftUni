16aaf36a5174972e04a64d4bed314aef18318fbe
Atanas809 <atanas2380@abv.bg>
2022-04-16 12:23:05 +0300

    Added ne file

---
+++ Python Fundamentals/Functions - Lab/grades.py
@@ -0,0 +1,17 @@
+# Задача 2:
+
+def grade(current_grade):
+
+    if 2 <= current_grade <= 2.99:
+        print("Fail")
+    elif current_grade <= 3.49:
+        print("Poor")
+    elif current_grade <= 4.49:
+        print("Good")
+    elif current_grade <= 5.49:
+        print("Very Good")
+    elif current_grade <= 6:
+        print("Excellent")
+
+grade(float(input()))
+

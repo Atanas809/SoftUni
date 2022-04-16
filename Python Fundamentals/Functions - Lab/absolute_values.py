b9ff8db24eec958dde625ea525ea27071283e4c7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-16 12:25:24 +0300

    Update absolute_values.py

--- Python Fundamentals/Functions - Lab/absolute_values.py
+++ Python Fundamentals/Functions - Lab/absolute_values.py
@@ -3,6 +3,6 @@ numbers = (list(map(float, input().split())))
 absolute_values = list()
 
 for i in numbers:
-    absolute_values.append(abs(a))
+    absolute_values.append(abs(i))
 
 print(absolute_values)

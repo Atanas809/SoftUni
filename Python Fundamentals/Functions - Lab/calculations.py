a598a5c7764ffaba86e231c45f9d22e5ece6ba56
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-16 12:26:53 +0300

    Update calculations.py

--- Python Fundamentals/Functions - Lab/calculations.py
+++ Python Fundamentals/Functions - Lab/calculations.py
@@ -1,16 +1,16 @@
 def calc(operator, first_num, second_num):
 
     if operator == "multiply":
-        return first_num * s_num
+        return first_num * second_num
     elif operator == "divide":
-        return first_num // s_num
+        return first_num // second_num
     elif operator == "add":
-        return first_num + s_num
+        return first_num + second_num
     elif operator == "subtract":
-        return first_num - s_num
+        return first_num - second_num
 
 operator = input()
 f_num = int(input())
 s_num = int(input())
 
-print(calc(operator, first_num, s_num))
+print(calc(operator, first_num, second_num))

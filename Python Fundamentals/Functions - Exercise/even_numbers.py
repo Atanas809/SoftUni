760a06829cf89219c3220c5d536a693a0feeabfb
Atanas809 <atanas2380@abv.bg>
2022-04-18 11:33:01 +0300

    Added new file

---
+++ Python Fundamentals/Functions - Exercise/even_numbers.py
@@ -0,0 +1,13 @@
+# Задача 5:
+
+def filtered():
+
+    numbers = list(map(int, input().split()))
+
+    even_nums = list(filter(lambda x: x % 2 == 0, numbers))
+
+    return even_nums
+
+print(filtered())
+
+

db6a1b14e60a52b1bc54ea27375a6497340218fa
Atanas809 <atanas2380@abv.bg>
2022-04-18 11:33:33 +0300

    Added new file

---
+++ Python Fundamentals/Functions - Exercise/factorial_division.py
@@ -0,0 +1,17 @@
+# Задача 12:
+
+def division(number):
+
+    fact = 1
+
+    for x in range(1, number + 1):
+        fact *= x
+
+    return fact
+
+f_num = int(input())
+s_num = int(input())
+
+result = division(f_num) / division(s_num)
+
+print(f"{result:.2f}")

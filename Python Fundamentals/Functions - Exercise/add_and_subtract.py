962f23ea2f78d8e633ee8004328f1540e9ccd966
Atanas809 <atanas2380@abv.bg>
2022-04-18 11:31:56 +0300

    Added ne file

---
+++ Python Fundamentals/Functions - Exercise/add_and_subtract.py
@@ -0,0 +1,17 @@
+# Задача 2:
+
+def subtract(result, c):
+
+    return result - c
+
+def sum_numbers():
+
+    a = int(input())
+    b = int(input())
+    c = int(input())
+
+    result = a + b
+
+    return subtract(result, c)
+
+print(sum_numbers())

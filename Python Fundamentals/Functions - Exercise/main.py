3a3fc43e5962ecba3c42418ab2077d77acf9d2a2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-19 11:37:01 +0300

    Update main.py

--- Python Fundamentals/Functions - Exercise/main.py
+++ Python Fundamentals/Functions - Exercise/main.py
@@ -3,10 +3,10 @@ def factorial(num):
     if num >= 1:
         for i in range(1, num + 1):
             f = f * i
-    return f
+#     return f
 
-number1 = int(input())
-number2 = int(input())
+# number1 = int(input())
+# number2 = int(input())
 
 result = factorial(number1) / factorial(number2)
 

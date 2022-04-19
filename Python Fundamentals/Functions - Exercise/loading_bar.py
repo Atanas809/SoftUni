dcc575781b98e32f52efd82fafe83c92d316f183
Atanas809 <atanas2380@abv.bg>
2022-04-19 11:33:29 +0300

    Added new file

---
+++ Python Fundamentals/Functions - Exercise/loading_bar.py
@@ -0,0 +1,19 @@
+# Задача 11:
+def bar(number):
+    percents = (number // 10) * "%"
+    dots = (10 - len(percents)) * "."
+
+    return f"[{percents}{dots}]"
+
+def loading():
+
+    number = int(input())
+
+    if number == 100:
+        print(f"100% Complete!")
+        print("[%%%%%%%%%%]")
+    else:
+        print(f"{number}% {bar(number)}")
+        print("Still loading...")
+
+loading()

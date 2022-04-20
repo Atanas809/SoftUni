4d294e438b5b246a93ce364512c4d23fe244f025
Atanas809 <atanas2380@abv.bg>
2022-04-20 12:01:08 +0300

    Added new file

---
+++ Python Fundamentals/Functions - Exercise/smallest_of_three_numbers.py
@@ -0,0 +1,15 @@
+# Задача 1:
+
+def smallest():
+
+    first_number = int(input())
+    second_number = int(input())
+    third_number = int(input())
+
+    my_list = [first_number, second_number, third_number]
+
+    smallest_number = min(my_list)
+
+    return smallest_number
+
+print(smallest())

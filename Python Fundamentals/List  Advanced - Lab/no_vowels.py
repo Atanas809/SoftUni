7f8ff60bc773bf9151eb11fb588037a81fb8c4d2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-21 12:28:34 +0300

    Update no_vowels.py

--- Python Fundamentals/List  Advanced - Lab/no_vowels.py
+++ Python Fundamentals/List  Advanced - Lab/no_vowels.py
@@ -2,4 +2,4 @@ text = input()
 
 vowels = ['a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I']
 
-print(f"{''.join([xfor x in text if x not in vowels])}")
+print(f"{''.join([x for x in text if x not in vowels])}")

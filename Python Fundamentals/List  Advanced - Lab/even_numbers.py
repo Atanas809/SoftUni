74ed2f1a948ebdc3983fb0c979f3a2b0bdbaa91c
Atanas809 <atanas2380@abv.bg>
2022-04-21 12:24:13 +0300

    Added ne file

---
+++ Python Fundamentals/List  Advanced - Lab/even_numbers.py
@@ -0,0 +1,14 @@
+# Задача 6:
+
+nums = list(map(int, input().split(", ")))
+
+indices = list()
+
+index = 0
+
+for x in nums:
+    if x % 2 == 0:
+        indices.append(index)
+    index += 1
+
+print(indices)

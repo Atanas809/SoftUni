b8bd80e636fb048d25ca8e49d47c4357cf8d93ee
Atanas809 <atanas2380@abv.bg>
2022-04-21 12:25:44 +0300

    Added new file

---
+++ Python Fundamentals/List  Advanced - Lab/palindrome_strings.py
@@ -0,0 +1,15 @@
+# Задача 4:
+
+words = input().split()
+
+searched_word = input()
+
+palindromes = list()
+
+for x in words:
+    if x == x[::-1]:
+        palindromes.append(x)
+
+print(palindromes)
+print(f"Found palindrome {words.count(searched_word)} times")
+

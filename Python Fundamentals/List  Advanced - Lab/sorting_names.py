5fb1538d23ad4a5120978eb1f28517a3e4d52a7f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-22 10:06:37 +0300

    Update sorting_names.py

--- Python Fundamentals/List  Advanced - Lab/sorting_names.py
+++ Python Fundamentals/List  Advanced - Lab/sorting_names.py
@@ -1,5 +1,6 @@
 # 1: Sorting names by ascending order
 # 2: Sorting names by their length
+# 3: If two or more names are the same length, they need to be sorted in ascending order.
 
 names = sorted(input().split(", "))
 

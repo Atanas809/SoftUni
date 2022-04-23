57a694ff5015ce32e9334088922f6b76413161a9
Atanas809 <atanas2380@abv.bg>
2022-04-23 12:31:48 +0300

    Added new file

---
+++ Python Fundamentals/List Advanced - Exercise/decipher_this.py
@@ -0,0 +1,29 @@
+# Задача 8:
+import re
+
+def decipher():
+    secret_message = input().split()
+
+    num_expression = r"[0-9]+"
+    letters_expression = r"[a-zA-Z]+"
+
+    message = list()
+
+    for x in secret_message:
+
+        number = re.findall(num_expression, x)
+        letters = re.findall(letters_expression, x)
+
+        current_num = int(number[0])
+        current_letter = letters[0]
+
+        if len(current_letter) > 1:
+            current_letter = current_letter[-1] + current_letter[1:-1] + current_letter[0]
+
+        output = chr(int(current_num)) + current_letter
+
+        message.append(output)
+
+    print(' '.join(message))
+
+decipher()

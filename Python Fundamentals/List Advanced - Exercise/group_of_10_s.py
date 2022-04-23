cf9ae86df3d3e88cb25f50fa21effee3730a093f
Atanas809 <atanas2380@abv.bg>
2022-04-23 12:33:27 +0300

    Added new file

---
+++ Python Fundamentals/List Advanced - Exercise/group_of_10_s.py
@@ -0,0 +1,16 @@
+# Задача 7:
+
+numbers = list(map(int, input().split(", ")))
+
+group = 10
+
+while len(numbers) != 0:
+    current_group = [x for x in numbers if x <= group]
+
+    for x in current_group:
+        numbers.remove(x)
+
+    print(f"Group of {group}'s: {current_group}")
+
+    group += 10
+

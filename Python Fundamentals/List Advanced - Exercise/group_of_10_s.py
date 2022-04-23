9db9251c0770dcb670dfcb21da468358adb38103
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-23 12:36:07 +0300

    Update group_of_10_s.py

--- Python Fundamentals/List Advanced - Exercise/group_of_10_s.py
+++ Python Fundamentals/List Advanced - Exercise/group_of_10_s.py
@@ -3,7 +3,7 @@ numbers = list(map(int, input().split(", ")))
 group = 10
 
 while len(numbers) != 0:
-    current_group = [x for x in numbers if x <= group]
+    current_group = [i for i in numbers if i <= group]
 
     for x in current_group:
         numbers.remove(x)

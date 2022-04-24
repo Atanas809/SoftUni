a81be7729e23f93b30b3681f8f6d0dc1e1972be4
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-24 12:04:23 +0300

    Update currency_converter.py

--- Python Fundamentals/Forex converter/currency_converter.py
+++ Python Fundamentals/Forex converter/currency_converter.py
@@ -1,17 +1,3 @@
-# Web-automation:
-
-import webbrowser as wb
-
-def web_automation():
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    URLS = ('softuni.bg', 'stackoverflow.com', 'github.com')
-
-    for url in URLS:
-        print("Opening..." + url)
-        wb.get(chrome_path).open(url)
-
-web_automation()
-
 # Forex converter:
 
 from forex_python.converter import CurrencyRates

11eec9f7023593c6da630a7cb588f17fe81c09a1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-24 12:09:22 +0300

    Update currency_converter.py

--- Python Fundamentals/Forex converter/currency_converter.py
+++ Python Fundamentals/Forex converter/currency_converter.py
@@ -12,5 +12,5 @@ print(amount, from_currency, "<-----|------>", to_currency)
 
 result = c.convert(from_currency, to_currency, amount)
 
-print(f"{result:.2f}")
+print(f"{result:.2f} {to_currency}")
 

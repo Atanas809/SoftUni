4fa54089589c86ed3bf5175bd81588cd72392095
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-24 12:01:15 +0300

    Update main.py

--- Python Fundamentals/List Advanced - Exercise/main.py
+++ Python Fundamentals/List Advanced - Exercise/main.py
@@ -14,17 +14,17 @@
 
 # Forex converter:
 
-from forex_python.converter import CurrencyRates
+# from forex_python.converter import CurrencyRates
 
-c = CurrencyRates()
+# c = CurrencyRates()
 
-amount = int(input('Enter amount: '))
-from_currency = input('From currency: ').upper()
-to_currency = input('To currency: ').upper()
+# amount = int(input('Enter amount: '))
+# from_currency = input('From currency: ').upper()
+# to_currency = input('To currency: ').upper()
 
-print(amount, from_currency, "<-----|------>", to_currency)
+# print(amount, from_currency, "<-----|------>", to_currency)
 
-result = c.convert(from_currency, to_currency, amount)
+# result = c.convert(from_currency, to_currency, amount)
 
-print(f"{result:.2f}")
+# print(f"{result:.2f}")
 

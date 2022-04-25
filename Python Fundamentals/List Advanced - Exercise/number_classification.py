8e6c205ce7c3127618c10cd8f04fe9773ce25970
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-25 12:42:28 +0300

    Update number_classification.py

--- Python Fundamentals/List Advanced - Exercise/number_classification.py
+++ Python Fundamentals/List Advanced - Exercise/number_classification.py
@@ -3,10 +3,10 @@ numbers = list(map(int, input().split(", ")))
 
 positive_numbers = [str(x) for x in numbers if x >= 0]
 negative_numbers = [str(x) for x in numbers if x < 0]
-even = [str(x) for x in numbers if x % 2 == 0]
+even_numbers = [str(x) for x in numbers if x % 2 == 0]
 odd = [str(x) for x in numbers if x % 2 != 0]
 
 print(f"Positive: {', '.join(positive_numbers)}")
 print(f"Negative: {', '.join(negative_numbers)}")
-print(f"Even: {', '.join(even)}")
+print(f"Even: {', '.join(even_numbers)}")
 print(f"Odd: {', '.join(odd)}")

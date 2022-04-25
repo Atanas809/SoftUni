74965e7827430f173df3c0df9be88ea7c4e29e31
Atanas809 <atanas2380@abv.bg>
2022-04-25 12:39:56 +0300

    Added new file

---
+++ Python Fundamentals/List Advanced - Exercise/number_classification.py
@@ -0,0 +1,14 @@
+# Задача 4:
+
+numbers = list(map(int, input().split(", ")))
+
+
+positives = [str(x) for x in numbers if x >= 0]
+negatives = [str(x) for x in numbers if x < 0]
+even = [str(x) for x in numbers if x % 2 == 0]
+odd = [str(x) for x in numbers if x % 2 != 0]
+
+print(f"Positive: {', '.join(positives)}")
+print(f"Negative: {', '.join(negatives)}")
+print(f"Even: {', '.join(even)}")
+print(f"Odd: {', '.join(odd)}")

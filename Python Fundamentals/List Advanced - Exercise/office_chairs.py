3ef838c0faf0f9cf98037cc87283d4352783b06f
Atanas809 <atanas2380@abv.bg>
2022-04-25 12:40:21 +0300

    Added new file

---
+++ Python Fundamentals/List Advanced - Exercise/office_chairs.py
@@ -0,0 +1,22 @@
+# Задача 5:
+
+number_of_rooms = int(input())
+
+free_chairs = 0
+condition = True
+
+for x in range(1, number_of_rooms + 1):
+    info = input().split()
+
+    chairs = len(info[0])
+    people = int(info[1])
+
+    if chairs < people:
+        diff = abs(chairs - people)
+        print(f"{diff} more chairs needed in room {x}")
+        condition = False
+    else:
+        free_chairs += abs(chairs - people)
+
+if condition:
+    print(f"Game On, {free_chairs} free chairs left")

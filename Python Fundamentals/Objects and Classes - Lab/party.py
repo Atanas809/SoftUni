44005de84165355bfd040b996a72eaadf3a5c54d
Atanas809 <atanas2380@abv.bg>
2022-04-27 11:52:20 +0300

    Added new file

---
+++ Python Fundamentals/Objects and Classes - Lab/party.py
@@ -0,0 +1,27 @@
+# Задача 2:
+
+class Party:
+
+    def __init__(self):
+        self.people = list()
+
+    def going(self, name):
+
+        while name != "End":
+
+            self.people.append(name)
+
+            name = input()
+
+        return self.people
+
+party = Party()
+
+names = input()
+
+people_going = party.going(names)
+
+print(f"Going: {', '.join(people_going)}")
+print(f"Total: {len(people_going)}")
+
+

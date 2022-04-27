f85247db0ea1ac20186b3f48d605969adc096d3c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-27 12:02:13 +0300

    Update pc_store.py

--- Python Basics/Exam Preparation - Python Basics/pc_store.py
+++ Python Basics/Exam Preparation - Python Basics/pc_store.py
@@ -9,9 +9,9 @@ video_cards_in_leva = video_cards_in_dollars * 1.57
 ram_in_leva = (ram_in_dollars * 1.57) * number_of_ram
 
 processor_discount = processor_in_leva * discount
-video_card_discount = video_card_in_leva * discount
+video_cards_discount = video_cards_in_leva * discount
 
-total_leva = (processor_in_leva - processor_discount) + (video_card_in_leva - video_card_discount) + ram_in_leva
+total_leva = (processor_in_leva - processor_discount) + (video_cards_in_leva - video_cards_discount) + ram_in_leva
 
 print(f"Money needed - {total_leva:.2f} leva.")
 

2a63a77a279cb11e415ec9cc486b6e0210b69b19
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-30 18:53:49 +0300

    Update weapon.py

--- Python Fundamentals/Objects and Classes - Exercise/weapon.py
+++ Python Fundamentals/Objects and Classes - Exercise/weapon.py
@@ -13,3 +13,13 @@ Class Weapon:
   def __repr__(self):
 
         return f"Remaining bullets: {self.bullets}"
+    
+weapon = Weapon(5)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)

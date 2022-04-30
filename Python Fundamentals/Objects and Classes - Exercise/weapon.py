489c6ce643851dd97130d942568e2b62f45a80c9
Atanas809 <atanas2380@abv.bg>
2022-04-30 18:51:26 +0300

    Added new file

---
+++ Python Fundamentals/Objects and Classes - Exercise/weapon.py
@@ -0,0 +1,13 @@
+import qrcode
+
+link = "https://github.com/Atanas809"
+
+qr = qrcode.QRCode(
+    version=3,
+    box_size=3,
+    border=4)
+
+qr.add_data(link)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='red')
+img.save('qrcode001.png')

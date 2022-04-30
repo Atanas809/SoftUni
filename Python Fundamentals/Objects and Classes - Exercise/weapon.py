09381ccab3fbd84a3639c6af1830d3f55884681c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-04-30 18:54:34 +0300

    Update weapon.py

--- Python Fundamentals/Objects and Classes - Exercise/weapon.py
+++ Python Fundamentals/Objects and Classes - Exercise/weapon.py
@@ -1,17 +1,17 @@
-Class Weapon:
-  
-  def __init__(self, bullets: int):
+class Weapon:
+
+    def __init__(self, bullets: int):
         self.bullets = bullets
-      
-  def shoot(self):
+
+    def shoot(self):
 
         if self.bullets > 0:
             self.bullets -= 1
             return "shooting..."
         else:
             return "no bullets left"
-          
-  def __repr__(self):
+
+    def __repr__(self):
 
         return f"Remaining bullets: {self.bullets}"
 # Examples:

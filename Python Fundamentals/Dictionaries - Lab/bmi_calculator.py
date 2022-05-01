68981c190eaf2cc577f25d70497d84eecb3a102d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-01 21:23:42 +0300

    Update bmi_calculator.py

--- Python Fundamentals/Dictionaries - Lab/bmi_calculator.py
+++ Python Fundamentals/Dictionaries - Lab/bmi_calculator.py
@@ -1,10 +1,10 @@
 def bmi_calculator():
 
-    height = float(input("Enter your height in cm: "))
-    weight = float(input("Enter your weight in kg: "))
+#     height = float(input("Enter your height in cm: "))
+#     weight = float(input("Enter your weight in kg: "))
 
-    height = height / 100
-    BMI = weight / (height * height)
+#     height = height / 100
+#     BMI = weight / (height * height)
 
     print(f"Your Body Mass Index is: {BMI:.2f}")
 

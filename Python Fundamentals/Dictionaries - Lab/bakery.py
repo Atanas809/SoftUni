3b87849e90ad56bf2c3427e228ed53b4b5377eba
Atanas809 <atanas2380@abv.bg>
2022-05-01 21:18:51 +0300

    Added new file

---
+++ Python Fundamentals/Dictionaries - Lab/bakery.py
@@ -0,0 +1,14 @@
+# Задача 1:
+
+data = input().split()
+
+my_dict = dict()
+
+for x in range(0, len(data) - 1, 2):
+
+    product = data[x]
+    quantity = int(data[x + 1])
+
+    my_dict[product] = quantity
+
+print(my_dict)

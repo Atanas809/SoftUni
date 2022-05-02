dae98771d15e8fbf7ffe884e55dc1021ef99cec0
Atanas809 <atanas2380@abv.bg>
2022-05-02 10:09:29 +0300

    Added new file

---
+++ Python Fundamentals/Dictionaries - Lab/odd_occurrences.py
@@ -0,0 +1,17 @@
+# Задача 6:
+
+data = input().lower().split()
+
+my_dict = dict()
+
+for x in data:
+
+    if x not in my_dict.keys():
+        my_dict[x] = 1
+    else:
+        my_dict[x] += 1
+
+for key, value in my_dict.items():
+
+    if value % 2 != 0:
+        print(key, end=" ")

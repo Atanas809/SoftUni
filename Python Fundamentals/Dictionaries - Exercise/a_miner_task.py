9538229d4bff13bd9b3b452d8dd60236a099bea7
Atanas809 <atanas2380@abv.bg>
2022-05-03 11:05:23 +0300

    Added new file

---
+++ Python Fundamentals/Dictionaries - Exercise/a_miner_task.py
@@ -0,0 +1,27 @@
+# Задача 2:
+
+def output(my_dict):
+
+    for key, value in my_dict.items():
+        print(f"{key} -> {value}")
+
+def resources():
+
+    info = input()
+
+    my_dict = dict()
+
+    while info != "stop":
+
+        quantity = int(input())
+
+        if info not in my_dict.keys():
+            my_dict[info] = quantity
+        else:
+            my_dict[info] += quantity
+
+        info = input()
+
+    output(my_dict)
+
+resources()

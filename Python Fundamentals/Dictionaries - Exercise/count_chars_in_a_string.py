0d9bbc819625483d3be0f7adbb425ecbaab8872a
Atanas809 <atanas2380@abv.bg>
2022-05-04 11:22:45 +0300

    Added new file

---
+++ Python Fundamentals/Dictionaries - Exercise/count_chars_in_a_string.py
@@ -0,0 +1,29 @@
+# Задача 1:
+
+# Solution without library:
+
+text = input()
+
+my_dict = dict()
+
+for x in text:
+    if x != " ":
+        if x not in my_dict.keys():
+            my_dict[x] = 1
+        else:
+            my_dict[x] += 1
+
+for key, value in my_dict.items():
+    print(f"{key} -> {value}")
+
+# Solution with library collections (Counter):
+
+from collections import Counter
+
+info = input()
+
+my_dict = dict(Counter(info))
+
+for key, value in my_dict.items():
+    if key != " ":
+        print(f"{key} -> {value}")

15729132b7a20058e139d17e91d949ad9c48bc9c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-04 11:29:57 +0300

    Update currency_converter.py

--- Python Fundamentals/Forex converter/currency_converter.py
+++ Python Fundamentals/Forex converter/currency_converter.py
@@ -8,7 +8,7 @@ amount = float(input('Enter your amount: '))
 from_currency = input('From currency: ').upper()
 to_currency = input('To currency: ').upper()
 
-print(amount, from_currency, "<-----|------>", to_currency)
+print(amount, from_currency, "<---|--->", to_currency)
 
 result = c.convert(from_currency, to_currency, amount)
 

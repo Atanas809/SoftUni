7ec9ba6a897fec5640b7a551064cb945405bf1a0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-05 12:26:23 +0300

    Update automatic_tab_opener.py

--- My Projects/Automatic tap opener/automatic_tab_opener.py
+++ My Projects/Automatic tap opener/automatic_tab_opener.py
@@ -5,3 +5,9 @@ import webbrowser as wb
 def web_automation():
     chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
     URLS = ("softuni.bg", "judge.softuni.org", "youtube.com")
+    
+    for url in URLS:
+        print(f"Opening: {url}")
+        wb.get(chrome_path).open(url)
+
+web_automation()

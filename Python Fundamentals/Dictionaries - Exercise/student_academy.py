b66425e5c6ef226f87df197022aef7e810e01d97
Atanas809 <atanas2380@abv.bg>
2022-05-05 12:18:26 +0300

    Added new file

---
+++ Python Fundamentals/Dictionaries - Exercise/student_academy.py
@@ -0,0 +1,30 @@
+# Задача 9:
+
+def output(academy_grades):
+
+    for key, value in academy_grades.items():
+        average_grade = sum(value) / len(value)
+
+        if average_grade >= 4.50:
+            print(f"{key} -> {average_grade:.2f}")
+
+def academy():
+
+    counter = int(input())
+
+    academy_grades = dict()
+
+    for _ in range(counter):
+
+        name = input()
+        grade = float(input())
+
+        if name not in academy_grades.keys():
+            academy_grades[name] = list()
+
+        academy_grades[name].append(grade)
+
+
+    output(academy_grades)
+
+academy()

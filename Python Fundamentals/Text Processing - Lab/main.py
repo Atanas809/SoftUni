e45122fd394ba31e93135287c9dce1228f1941c0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-06 11:32:26 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Lab/main.py
+++ Python Fundamentals/Text Processing - Lab/main.py
@@ -1,10 +1,10 @@
 # First way:
 
-# command = input()
-#
-# while command != "end":
-#     rev_word = command[::-1]
-#
-#     print(f"{command} = {rev_word}")
-#
-#     command = input()
+command = input()
+
+while command != "end":
+    rev_word = command[::-1]
+
+    print(f"{command} = {rev_word}")
+
+    command = input()

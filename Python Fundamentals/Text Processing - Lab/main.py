35cf4a2213e8c77663bc6e0d4f0d48aa4dd85199
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-07 10:47:34 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Lab/main.py
+++ Python Fundamentals/Text Processing - Lab/main.py
@@ -3,3 +3,13 @@ text = input()
 digits = ""
 letters = ""
 symbols = ""
+
+for char in text:
+    if char.isdigit():
+        digits += char
+    elif char.isalpha():
+        letters += char
+    else:
+        symbols += char
+
+print(f"{digits}\n{letters}\n{symbols}")

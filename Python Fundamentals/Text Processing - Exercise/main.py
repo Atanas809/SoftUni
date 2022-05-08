9f2be3191e547b4395deb65173f5b385ff843a33
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-08 11:00:43 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -18,26 +18,5 @@ def output(standings):
 
 
 
-def result(my_dict):
-
-    for key, value in my_dict.items():
-        print(f"{key}: {len(value) // 2} participants")
-        sort_dic = dict()
-        for x in range(0, len(value), 2):
-            name = value[x]
-            points = value[x + 1]
-            if name not in sort_dic.values():
-                sort_dic[name] = points
-        output = sorted(sort_dic.values(), reverse=True)
-        counter = 1
-        for y in output:
-            for item in sort_dic.items():
-                if item[1] == y:
-                    print(f"{counter}. {item[0]} <::> {item[1]}")
-                    counter += 1
-                    del sort_dic[item[0]]
-                    break
-
-
 
 

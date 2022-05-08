439fded1f65998dd329e9701b0d6cf1c3183a4f8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-08 11:02:47 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -46,47 +46,6 @@ def judge():
     individual_standings = dict()
     names = list()
 
-    while data[0] != "no more time":
-        name = data[0]
-        course = data[1]
-        points = int(data[2])
-
-        if course in my_dict.keys() and name in names:
-            for key, value in my_dict.items():
-                if key == course and name in value:
-                    for x in range(0, len(value), 2):
-                        user = value[x]
-                        num = value[x + 1]
-                        if user == name:
-                            if num < points:
-                                my_dict[course][x + 1] = points
-                                break
-
-        if course not in my_dict.keys():
-            my_dict[course] = list()
-            my_dict[course].append(name)
-            my_dict[course].append(points)
-        else:
-            if name not in my_dict[course]:
-                my_dict[course].append(name)
-                my_dict[course].append(points)
-
-        if name not in names:
-            names.append(name)
-
-        if name not in individual_standings.keys():
-            individual_standings[name] = list()
-            individual_standings[name].append(points)
-        else:
-            for key, value in my_dict.items():
-                if course == key and name in my_dict[course]:
-                    if individual_standings[name][0] < points:
-                        individual_standings[name][0] = points
-                        break
-                else:
-                    if course != key and name in my_dict[course]:
-                        individual_standings[name][0] += points
-                        break
 
 
 

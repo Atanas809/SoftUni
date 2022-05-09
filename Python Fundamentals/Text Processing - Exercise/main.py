bb569eab130b92c0e3a612beafe53678e8aaed6b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-09 10:36:45 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -4,4 +4,13 @@ output = ""
 
 index = 0
 
+for x in text:
+    if len(output) == 0:
+        output += x
+        index += 1
+    else:
+        if output[index - 1] != x:
+            output += x
+            index += 1
 
+print(output)

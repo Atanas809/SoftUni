b56eb81b25dcb24dbb4c77b9fc4895424853e1be
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-09 10:33:46 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -1,1 +1,9 @@
 def extraction():
+
+    file_data = input().split("\\")
+
+    file_name = file_data[-1].split(".")
+
+    print(f"File name: {file_name[0]}\nFile extension: {file_name[1]}")
+
+extraction()

538fce66aa0579319099a866e8d2e7dfaf69fb98
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-10 11:45:47 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -28,3 +28,24 @@ def rage():
     index = 0
 
     condition = True
+
+    while index <= len(data) - 1:
+
+        if data[index].isdigit():
+            while data[index].isdigit():
+                if index < len(data) - 1:
+                    index += 1
+                else:
+                    text.append(data[:index + 1])
+                    condition = False
+                    break
+
+            if condition:
+                text.append(data[:index])
+                data = data.replace(data[:index], "", 1)
+                index = 0
+                continue
+        else:
+            unique_symbols.add(data[index].lower())
+
+        index += 1

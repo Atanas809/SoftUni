e51d5a508f5e79861adbe4c009ddecf0ab85e679
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-10 11:45:01 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -2,3 +2,14 @@ def output(unique_symbols, text):
 
     result = ""
 
+    for t in text:
+        number = ""
+        chars = ""
+
+        for x in t:
+            if x.isdigit():
+                number += x
+            else:
+                chars += x.upper()
+
+        result += chars * int(number)

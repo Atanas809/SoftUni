53c6d8b8be00b41e8e805955185be7c45c18c8ed
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-11 11:08:07 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -1,19 +1,1 @@
 
-                if course == key and name in my_dict[course]:
-                    if individual_standings[name][0] < points:
-                        individual_standings[name][0] = points
-                        break
-                else:
-                    if course != key and name in my_dict[course]:
-                        individual_standings[name][0] += points
-                        break
-
-
-
-        data = input().split(" -> ")
-
-    my_dict = result(my_dict)
-    individual_standings = output(individual_standings)
-
-judge()
-

52b0ecbbef76100be7bb1f1af0dee8a309769123
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-11 11:02:45 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -1,3 +1,15 @@
 import re
 
 def valid_users():
+
+    usernames = input().split(", ")
+
+    expression = r"^(?P<valid>[a-zA-Z0-9_-]{3,16})$"
+
+    for x in usernames:
+        matches = re.finditer(expression, x)
+
+        for match in matches:
+            print(match.group("valid"))
+
+valid_users()

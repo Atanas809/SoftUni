34f0524d11c7b34580604dee0b25f57e65a7e6bd
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-11 11:08:25 +0300

    Update main.py

--- Python Fundamentals/Text Processing - Exercise/main.py
+++ Python Fundamentals/Text Processing - Exercise/main.py
@@ -1,1 +1,54 @@
+def is_winning(ticket, symbols):
+
+    l_side = ticket[:10]
+    r_side = ticket[10:]
+
+    left = ""
+    right = ""
+
+
+    for s in symbols:
+
+        if s in l_side:
+            if s * 6 in l_side:
+                left = s * 6
+
+            if s * 7 in l_side:
+                left = s * 7
+
+            if s * 8 in l_side:
+                left = s * 8
+
+            if s * 9 in l_side:
+                left = s * 9
+
+        if s in r_side:
+            if s * 6 in r_side:
+                right = s * 6
+
+            if s * 7 in r_side:
+                right = s * 7
+
+            if s * 8 in r_side:
+                right = s * 8
+
+            if s * 9 in r_side:
+                right = s * 9
+
+    if left != "" and right != "":
+
+        if left == right:
+            print(f'ticket "{ticket}" - {len(left)}{left[0]}')
+
+        else:
+            diff = abs(len(left) - len(right))
+
+            if len(left) > len(right):
+                print(f'ticket "{ticket}" - {len(right)}{right[0]}')
+
+            elif len(left) < len(right):
+                print(f'ticket "{ticket}" - {len(left)}{left[0]}')
+
+    else:
+        print(f'ticket "{ticket}" - no match')
 

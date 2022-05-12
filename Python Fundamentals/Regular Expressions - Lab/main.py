53426cdc0c15c244fd0ff59007df5ce2305b9804
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-12 13:02:24 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Lab/main.py
+++ Python Fundamentals/Regular Expressions - Lab/main.py
@@ -7,3 +7,6 @@ expression = r"\+359([ |-])2\1([0-9]{3})\1([0-9]{4})\b"
 matches = re.finditer(expression, phones)
 
 valid_phones = list()
+
+for match in matches:
+    valid_phones.append(match[0])

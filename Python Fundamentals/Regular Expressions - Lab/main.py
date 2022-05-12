dec2ec719ab8f66dc94d5279aff0cd9466dca494
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-12 12:59:54 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Lab/main.py
+++ Python Fundamentals/Regular Expressions - Lab/main.py
@@ -1,5 +1,1 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 

2d73ad3aafd8299d67e1498351bac8195be2e57a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-13 11:42:37 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Lab/main.py
+++ Python Fundamentals/Regular Expressions - Lab/main.py
@@ -5,3 +5,8 @@ dates = input()
 expression = r"\b(?P<day>\d{2})([-.\/])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4})\b"
 
 matches = re.finditer(expression, dates)
+
+for match in matches:
+    day = match.group("day")
+    month = match.group("month")
+    year = match.group("year")

d7a2c0996ea55ea898ee4ceb626bc6ed7956f497
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-13 11:47:02 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Lab/main.py
+++ Python Fundamentals/Regular Expressions - Lab/main.py
@@ -7,3 +7,6 @@ expression = r"(^|(?<=\s))(-)?([0]|[1-9][0-9]*)\.?([0-9]+)?($|(?=\s))"
 matches = re.finditer(expression, numbers)
 
 valid = list()
+
+for match in matches:
+    valid.append(match[0])

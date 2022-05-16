46342b7072ca10e87a9a18cff0aa8516d42a0cda
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-16 10:39:19 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Exercise/main.py
+++ Python Fundamentals/Regular Expressions - Exercise/main.py
@@ -11,3 +11,8 @@ furniture = list()
 while data != "Purchase":
 
     matches = re.finditer(expression, data)
+
+    for match in matches:
+        item = match.group("item")
+        price = float(match.group("price"))
+        quantity = int(match.group("quantity"))

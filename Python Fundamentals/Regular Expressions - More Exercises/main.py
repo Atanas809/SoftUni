78c1903837e66337108b567770d4c6545610b4d4
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-17 10:58:46 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - More Exercises/main.py
+++ Python Fundamentals/Regular Expressions - More Exercises/main.py
@@ -12,3 +12,8 @@ def output(my_dict):
         current_name = [k for k, v in my_dict.items() if v == best_points[x]]
 
         best_results.append(current_name[0])
+
+    print(f"""1st place: {best_results[0]}
+2nd place: {best_results[1]}
+3rd place: {best_results[2]}
+""")

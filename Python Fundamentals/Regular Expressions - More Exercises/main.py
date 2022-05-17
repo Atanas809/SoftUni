b9595e0bc6baf32deecdaece2b005c42a4b2258f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-17 10:57:24 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - More Exercises/main.py
+++ Python Fundamentals/Regular Expressions - More Exercises/main.py
@@ -12,3 +12,9 @@ def race():
 
         current_name = names(data)
         current_points = points(data)
+
+        for name in participants:
+
+            if current_name == name.strip():
+                if current_name not in my_dict.keys():
+   

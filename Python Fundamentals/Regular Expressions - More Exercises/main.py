a2a440a9830f4858ed2c91e4cb7483d530811927
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-17 11:00:35 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - More Exercises/main.py
+++ Python Fundamentals/Regular Expressions - More Exercises/main.py
@@ -61,3 +61,7 @@ def race():
                     my_dict[current_name] = current_points
                 else:
                     my_dict[current_name] += current_points
+
+        data = input()
+
+    output(my_dict)

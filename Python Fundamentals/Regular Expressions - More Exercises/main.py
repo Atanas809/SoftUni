bd41d2e9a7fb15438888f199d38891876bc89d1d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-17 10:58:35 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - More Exercises/main.py
+++ Python Fundamentals/Regular Expressions - More Exercises/main.py
@@ -7,3 +7,8 @@ def output(my_dict):
     best_points = val_list[:3]
 
     best_results = list()
+
+    for x in range(0, len(best_points)):
+        current_name = [k for k, v in my_dict.items() if v == best_points[x]]
+
+        best_results.append(current_name[0])

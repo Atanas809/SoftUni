62eebd31cd1b41a70e32fc99607cba9249cf52b2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-19 16:41:38 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - More Exercises/main.py
+++ Python Fundamentals/Regular Expressions - More Exercises/main.py
@@ -4,3 +4,6 @@ def output(attack_planets, destroyed_planets):
     for key, value in attack_planets.items():
         value = sorted(value)
         print(f"Attacked planets: {len(value)}")
+        if len(value) > 0:
+            for planet in value:
+                print(f"-> {planet}")

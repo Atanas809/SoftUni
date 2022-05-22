e5fc6c35757f2966aea61d17b0c158f05c816b0a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-22 12:22:13 +0300

    Update main.py

--- Python Fundamentals/Lists Advanced - More Exercises/main.py
+++ Python Fundamentals/Lists Advanced - More Exercises/main.py
@@ -1,18 +1,3 @@
-def escape(current_maze):
-
-    for x in range(len(current_maze)):
-        if "k" in current_maze[x]:
-            index = current_maze[x].index("k")
-
-
-
-def maze():
-
-    counter = int(input())
-
-    current_maze = list()
-
-    for _ in range(counter):
 
         data = input()
 

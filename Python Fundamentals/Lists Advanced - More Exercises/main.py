d01c38526344884f3d8da0e70cd5a4b7fc67a98f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-22 12:34:23 +0300

    Update main.py

--- Python Fundamentals/Lists Advanced - More Exercises/main.py
+++ Python Fundamentals/Lists Advanced - More Exercises/main.py
@@ -4,3 +4,8 @@ def battle(my_list, attacked):
 
     for x in attacked:
         data = x.split("-")
+        row = int(data[0])
+        col = int(data[1])
+        if my_list[row][col] - 1 == 0:
+            my_list[row][col] = 0
+            destroyed += 1

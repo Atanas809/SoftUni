c735b2f050f50742191930a73238199971ef1d3e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-24 11:09:59 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -3,3 +3,8 @@ prices = input()
 net_price = 0
 taxes = 0
 total_price = 0
+
+while prices != "special" and prices != "regular":
+    current_price = float(prices)
+    if current_price <= 0:
+        print("Invalid price!")

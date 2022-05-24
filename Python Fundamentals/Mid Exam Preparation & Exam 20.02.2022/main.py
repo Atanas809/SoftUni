c8b723a1ef5f6b84c73b372dfe313138a9c1c1d0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-24 11:06:18 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -1,3 +1,8 @@
 numbers = list(map(int, input().split(" ")))
 
 command = input().split(" ")
+
+while command[0] != "end":
+    if command[0] == "decrease":
+        numbers = list(map(lambda x: x - 1, numbers))
+        command = input().split(" ")

883c2bd5b44e003f995651f853838b2224f06ddd
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-24 11:06:32 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -6,3 +6,6 @@ while command[0] != "end":
     if command[0] == "decrease":
         numbers = list(map(lambda x: x - 1, numbers))
         command = input().split(" ")
+        continue
+    index1 = int(command[1])
+    index2 = int(command[2])

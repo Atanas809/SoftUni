391cc800016b18301c39d5974bee94a93efd4f46
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-26 10:41:45 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -4,7 +4,8 @@ counter = 0
 need_sleep = False
 
 while commands != "END":
-   if counter >= 5:
+      if counter >= 5:
         need_sleep = True
         print("You need extra sleep")
         break
+    elif commands == "coding" or commands == "movie":

00ecee2388b2e1083bb87469b26bbe40f0e23003
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-28 10:45:28 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -6,3 +6,7 @@ if "sand" in text:
     counter += text.count("sand")
 if "water" in text:
     counter += text.count("water")
+if "fish" in text:
+    counter += text.count("fish")
+if "sun" in text:
+    counter += text.count("sun")

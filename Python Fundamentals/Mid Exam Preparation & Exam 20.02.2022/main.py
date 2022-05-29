8037aaa7808ab686b1e8e04c3ca374e7b4efefa7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-05-29 21:16:57 +0300

    Update main.py

--- Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
+++ Python Fundamentals/Mid Exam Preparation & Exam 20.02.2022/main.py
@@ -5,3 +5,8 @@ students = int(input())
 price_flour = float(input())
 price_single_egg = float(input())
 price_apron = float(input())
+
+all_aprons = price_apron * (students + ceil(students * 0.2))
+
+total_price = all_aprons
+count_flour = 0

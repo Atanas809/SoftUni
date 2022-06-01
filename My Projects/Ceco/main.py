f46c82cf188cfbb74d05b0ded7ae30de361d4b45
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-01 23:53:55 +0300

    Update main.py

--- My Projects/Ceco/main.py
+++ My Projects/Ceco/main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+# Task 3:
+def count_alphabet(text):
+    my_dict = dict()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    for char in text:
+        if char not in my_dict.keys():
+            my_dict[char] = 0
+        my_dict[char] += 1
+
+    return my_dict
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Task 4:
+def substitute_word(text, word, replace_word):
+    text = text.replace(word, replace_word)
+
+    return text
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Task 5:
+def output(value):
+    first_name = value["firstname"]
+    last_name = value["lastname"]
+    points = int(value["points"])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return f"""Dear {first_name} {last_name},
+
+You have received {points} points for the exercises of session 9.
+
+Best wishes,
+Ivan ivanov"""
+
+
+def create_letter(students):
+
+    result = []
+
+    for key, value in students.items():
+        result.append(output(value))
+
+    return '\n'.join(result)
+
+wilhelm_tell = """
+A high, rocky shore of the lake of Lucerne opposite Schwytz.
+The lake makes a bend into the land; a hut stands at a short
+distance from the shore; the fisher boy is rowing about in his
+boat. Beyond the lake are seen the green meadows, the hamlets,
+and arms of Schwytz, lying in the clear sunshine. On the left
+are observed the peaks of the Hacken, surrounded with clouds; to
+the right, and in the remote distance, appear the Glaciers. The
+Ranz des Vaches, and the tinkling of cattle-bells, continue for
+some time after the rising of the curtain.
+"""
+
+students_info = {"126128": {"firstname": "Sara", "lastname": "Thomson", "points": 6},
+                 "928710": {"firstname": "John", "lastname": "Doe", "points": 2},
+                 "118979": {"firstname": "Emily", "lastname": "Jones", "points": 9},
+                 "652182": {"firstname": "Martin", "lastname": "Mueller", "points": 12},
+                 "526251": {"firstname": "Ella", "lastname": "Bauer", "points": 8}}
+
+print(create_letter(students_info))
+
+print(substitute_word(wilhelm_tell, "Glaciers", "Alps"))
+
+print(count_alphabet("abcdefghijklmnopqrstuvwxyz"))
+
+

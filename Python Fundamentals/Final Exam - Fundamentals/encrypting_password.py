f9440a589e5890c08ca18c6c5d7ca73420c70d91
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-01 10:06:23 +0300

    Update encrypting_password.py

--- Python Fundamentals/Final Exam - Fundamentals/encrypting_password.py
+++ Python Fundamentals/Final Exam - Fundamentals/encrypting_password.py
@@ -6,9 +6,8 @@ expression = r"^(\W+|\w+)(>)(?P<valid>[0-9]{3,}[|][a-z]{3,}[|][A-Z]{3,}[|][^\<\>
 
 for x in range(number):
     data = input()
-
     matches = re.finditer(expression, data)
-
+    
     output = ""
 
     for match in matches:

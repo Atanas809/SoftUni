a3e1aea9e778e47c605e11f5f3d1b11087260e4e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-02 10:02:59 +0300

    Update main.py

--- My Projects/Ceco/main.py
+++ My Projects/Ceco/main.py
@@ -1,19 +1,1 @@
 
-the right, and in the remote distance, appear the Glaciers. The
-Ranz des Vaches, and the tinkling of cattle-bells, continue for
-some time after the rising of the curtain.
-"""
-
-students_info = {"126128": {"firstname": "Sara", "lastname": "Thomson", "points": 6},
-                 "928710": {"firstname": "John", "lastname": "Doe", "points": 2},
-                 "118979": {"firstname": "Emily", "lastname": "Jones", "points": 9},
-                 "652182": {"firstname": "Martin", "lastname": "Mueller", "points": 12},
-                 "526251": {"firstname": "Ella", "lastname": "Bauer", "points": 8}}
-
-print(create_letter(students_info))
-
-print(substitute_word(wilhelm_tell, "Glaciers", "Alps"))
-
-print(count_alphabet("abcdefghijklmnopqrstuvwxyz"))
-
-

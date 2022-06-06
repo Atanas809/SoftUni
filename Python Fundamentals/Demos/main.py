4cd3085d1650b699bfe9cc4cbd78cd0d69b188f8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-06 12:54:11 +0300

    Update main.py

--- Python Fundamentals/Demos/main.py
+++ Python Fundamentals/Demos/main.py
@@ -7,3 +7,8 @@ def results(individuals):
 
     for x in sorted_dict:
         for key, value in individuals.items():
+            if value == x:
+                print(f"{counter}. {key} -> {x[0]}")
+                counter += 1
+                del individuals[key]
+                break

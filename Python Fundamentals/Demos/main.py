3a87a7b116a2c44b2fd840abac88d71d589a91dc
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-06 12:54:00 +0300

    Update main.py

--- Python Fundamentals/Demos/main.py
+++ Python Fundamentals/Demos/main.py
@@ -2,3 +2,8 @@ def results(individuals):
     print("Individual standings:")
 
     sorted_dict = sorted(individuals.values(), reverse=True)
+
+    counter = 1
+
+    for x in sorted_dict:
+        for key, value in individuals.items():

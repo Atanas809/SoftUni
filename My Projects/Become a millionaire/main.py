3739006b7adec55127eafd6ee4863f6ccf5bb1aa
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-07 11:37:08 +0300

    Update main.py

--- My Projects/Become a millionaire/main.py
+++ My Projects/Become a millionaire/main.py
@@ -1,2 +1,5 @@
 def next_question():
     print("\nLet's continue with you next question:")
+
+    print("""Someone who strongly asserts a position is often said to be "putting his" what down?
+A: Foot

7363725e20305042aafc303c9b25cad0a9240ebd
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-08 10:38:37 +0300

    Update main.py

--- Python Fundamentals/Regular Expressions - Lab/main.py
+++ Python Fundamentals/Regular Expressions - Lab/main.py
@@ -4,3 +4,8 @@ def final_result(current_title, current_content):
 
     current_title = current_title.strip()
     current_content = current_content.strip()
+
+    new_title = current_title.split(" ")
+    new_content = current_content.split(" ")
+
+    x = 0

461f49c588eb81489c386dc5a83e39c93ec783a8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-10 11:04:22 +0300

    Update search.py

--- Python Fundamentals/List - Basics/search.py
+++ Python Fundamentals/List - Basics/search.py
@@ -1,17 +1,12 @@
 number = int(input())
-
 searched_word = input()
-
 all_data = list()
-
 only_with_searched_word = list()
 
 for _ in range(number):
-
     current_text = input()
-
     all_data.append(current_text)
-
+    
     if searched_word in current_text:
         only_with_searched_word.append(current_text)
 

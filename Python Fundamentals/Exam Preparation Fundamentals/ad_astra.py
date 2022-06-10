f35f73684699a6bceed3cf69d67234d045d936f6
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-10 11:02:25 +0300

    Update ad_astra.py

--- Python Fundamentals/Exam Preparation Fundamentals/ad_astra.py
+++ Python Fundamentals/Exam Preparation Fundamentals/ad_astra.py
@@ -1,11 +1,8 @@
 import re
 
 data = input()
-
 expression = r"(#|\|)(?P<food>[a-zA-Z\s]+)\1(?P<ex_date>[\d]{2}\/[\d]{2}\/[\d]{2})\1(?P<calories>[\d]{1,5})\1"
-
 matches = re.finditer(expression, data)
-
 total_calories = 0
 
 output = list()

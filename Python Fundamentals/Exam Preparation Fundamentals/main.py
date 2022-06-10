5fb2c0db6d8cf5c2aa2fa6812c6a51d351123857
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-10 11:00:31 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -5,3 +5,5 @@ data = input()
 expression = r"(#|\|)(?P<food>[a-zA-Z\s]+)\1(?P<ex_date>[\d]{2}\/[\d]{2}\/[\d]{2})\1(?P<calories>[\d]{1,5})\1"
 
 matches = re.finditer(expression, data)
+
+total_calories = 0

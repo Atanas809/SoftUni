6bbc457998ea7cc04ade02dee16b6c91bde3d0ff
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-17 11:32:13 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,3 +1,10 @@
 def result(my_dict):
     for key, value in my_dict.items():
         print(f"{key}\n  HP: {my_dict[key][0]}\n  MP: {my_dict[key][1]}")
+
+        
+def output(my_dict):
+
+    command = input().split(" - ")
+
+    while command[0] != "End":

ff9105e499390153258f6f7639baddcc6bbc146d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-17 11:31:39 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,14 +1,1 @@
 
-    counter = int(input())
-
-    current_maze = list()
-
-    for _ in range(counter):
-
-        data = input()
-
-        current_maze.append(data)
-
-    escape(current_maze)
-
-maze()

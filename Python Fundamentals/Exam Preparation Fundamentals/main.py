e7b548ff99cb6b7b3d6f667adfdfd8ad7f84433f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-18 14:41:38 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,33 +1,4 @@
 
-    return current_points
-
-def names(data):
-
-    letters = r"[a-zA-Z]+"
-
-    matches = re.findall(letters, data)
-
-    current_name = ''.join(matches)
-
-    return current_name
-
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
-
-        current_name = names(data)
-        current_points = points(data)
-
-        for name in participants:
-
-            if current_name == name.strip():
-                if current_name not in my_dict.keys():
                     my_dict[current_name] = current_points
                 else:
                     my_dict[current_name] += current_points

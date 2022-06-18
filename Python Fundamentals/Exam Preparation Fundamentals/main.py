378672eea18f581cac2d38d0ef2b2be4bc73589c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-18 14:45:08 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,3 +1,9 @@
 def result(final_cars):
     for key, value in final_cars.items():
         print(f"{key} -> Mileage: {final_cars[key][0]} kms, Fuel in the tank: {final_cars[key][1]} lt.")
+
+def output(my_dict):
+
+    command = input().split(" : ")
+
+    while command[0] != "Stop":

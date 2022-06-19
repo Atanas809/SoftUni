8ddb66cdf86a1ce1b2f84f0aeeb79b234650999a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-19 12:07:20 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -2,3 +2,7 @@ def output(my_dict):
 
     if len(my_dict.keys()) > 0:
         print(f"Ahoy, Captain! There are {len(my_dict.keys())} wealthy settlements to go to:")
+        for key, value in my_dict.items():
+            print(f"{key} -> Population: {my_dict[key][0]} citizens, Gold: {my_dict[key][1]} kg")
+    else:
+        print("Ahoy, Captain! All targets have been plundered and destroyed!")

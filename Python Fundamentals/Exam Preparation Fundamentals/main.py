ce3506b3c91c6b4a08f4686f955ebb09d33bbf26
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-19 12:06:50 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,19 +1,4 @@
 
-
-    current_name = ''.join(matches)
-
-    return current_name
-
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
-
         current_name = names(data)
         current_points = points(data)
 

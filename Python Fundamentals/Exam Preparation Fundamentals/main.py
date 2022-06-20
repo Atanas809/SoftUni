cd64ecfad807dccac812a64cb83e21a8e4ded2b0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-20 10:32:56 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,3 +1,10 @@
 def result(my_dict):
 
     print("Plants for the exhibition:")
+
+    for key, value in my_dict.items():
+        plant = key
+        rarity = my_dict[plant][0]
+        average_rating = 0
+        if len(value) != 1:
+            average_rating += sum(value[1:]) / len(value[1:])

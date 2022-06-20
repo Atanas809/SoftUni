6a9779138c3f550805a1da2a131eb71fac693098
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-20 10:31:24 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,11 +1,5 @@
 import re
 
-def output(my_dict):
-
-    val_list = list(sorted(my_dict.values(), reverse=True))
-
-    best_points = val_list[:3]
-
     best_results = list()
 
     for x in range(0, len(best_points)):

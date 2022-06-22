c9464caee7f945da862e1efbcca07aac71cf4293
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-22 11:03:39 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -6,3 +6,6 @@ while command[0] != "Travel":
     if command[0] == "Add Stop":
         index = int(command[1])
         text = command[2]
+
+        if 0 <= index <= len(stops) - 1:
+            l_side = stops[:index]

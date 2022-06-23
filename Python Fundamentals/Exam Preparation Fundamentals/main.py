38f46bdd46dcb21c205850000994c49bab2297a8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-23 10:49:51 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,20 +1,5 @@
 
 
-def points(data):
-
-    digits = r"\d"
-
-    matches = re.findall(digits, data)
-
-    current_points = 0
-
-    for point in matches:
-        current_points += int(point)
-
-    return current_points
-
-def names(data):
-
     letters = r"[a-zA-Z]+"
 
     matches = re.findall(letters, data)

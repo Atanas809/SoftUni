6aa2ad5d322bc7a84f5373af193d34cf8c9a0093
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-23 10:50:00 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,21 +1,5 @@
 
 
-    letters = r"[a-zA-Z]+"
-
-    matches = re.findall(letters, data)
-
-    current_name = ''.join(matches)
-
-    return current_name
-
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
     while data != "end of race":
 
         current_name = names(data)

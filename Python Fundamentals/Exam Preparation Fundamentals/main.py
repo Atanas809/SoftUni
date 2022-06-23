8cf8bd48fbc3a14f8608884b6ad59f84c72d76aa
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-23 10:50:44 +0300

    Update main.py

--- Python Fundamentals/Exam Preparation Fundamentals/main.py
+++ Python Fundamentals/Exam Preparation Fundamentals/main.py
@@ -1,3 +1,8 @@
 def result(my_dict):
     for key, value in my_dict.items():
         print(f"{key} -> Composer: {my_dict[key][0]}, Key: {my_dict[key][1]}")
+
+
+def output(my_dict):
+
+    command = input().split("|")

c52fc8203491c7ab3428d2a21403435ec0443899
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-24 11:45:00 +0300

    Update main.py

--- My Projects/QR Generator/main.py
+++ My Projects/QR Generator/main.py
@@ -1,17 +1,4 @@
 
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
-
-        current_name = names(data)
-        current_points = points(data)
-
-        for name in participants:
-
-            if current_name == name.strip():
                 if current_name not in my_dict.keys():
                     my_dict[current_name] = current_points
                 else:

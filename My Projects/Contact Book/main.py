f2b1b61f5b1f1d8dfc095815777ee3301eb12310
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-25 12:40:08 +0300

    Update main.py

--- My Projects/Contact Book/main.py
+++ My Projects/Contact Book/main.py
@@ -1,11 +1,4 @@
 
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
 
         current_name = names(data)
         current_points = points(data)

c5e33572853fe69a5ef156006ef413d014fcaeca
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-26 10:29:19 +0300

    Update main.py

--- My Projects/Forex converter/main.py
+++ My Projects/Forex converter/main.py
@@ -9,3 +9,5 @@ from_currency = input('From currency: ').upper()
 to_currency = input('To currency: ').upper()
 
 print(amount, from_currency, "<----|---->", to_currency)
+
+result = c.convert(from_currency, to_currency, amount)

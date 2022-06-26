be378753ba35e05e9d9d4457a1f824d6ca6201f4
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-26 10:29:09 +0300

    Update main.py

--- My Projects/Forex converter/main.py
+++ My Projects/Forex converter/main.py
@@ -7,3 +7,5 @@ c = CurrencyRates()
 amount = float(input('Enter your amount: '))
 from_currency = input('From currency: ').upper()
 to_currency = input('To currency: ').upper()
+
+print(amount, from_currency, "<----|---->", to_currency)

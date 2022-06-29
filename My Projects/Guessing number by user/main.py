a8f41a9c54fc61374a389b9b96c1872b42946c18
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-29 11:53:24 +0300

    Update main.py

--- My Projects/Guessing number by user/main.py
+++ My Projects/Guessing number by user/main.py
@@ -1,12 +1,1 @@
 
-                if current_name not in my_dict.keys():
-                    my_dict[current_name] = current_points
-                else:
-                    my_dict[current_name] += current_points
-
-        data = input()
-
-    output(my_dict)
-
-race()
-

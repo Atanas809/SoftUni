8536e1d4fcaa9283b15f1d5cb7a43f5a5894559d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-06-30 10:38:29 +0300

    Update main.py

--- My Projects/Rock Paper Scissors/main.py
+++ My Projects/Rock Paper Scissors/main.py
@@ -5,3 +5,6 @@ def who_wins(player1, player2):
 
     if (player1 == "r" and player2 == "s") or\
         (player1 == "s" and player2 == "p") or\
+        (player1 == "p" and player2 == "r"):
+
+        return True

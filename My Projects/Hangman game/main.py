8935bb8e108691676157fbdeeb604ccd54aad7d2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-01 11:38:43 +0300

    Update main.py

--- My Projects/Hangman game/main.py
+++ My Projects/Hangman game/main.py
@@ -1,9 +1,4 @@
 
-                    my_dict[current_name] = current_points
-                else:
-                    my_dict[current_name] += current_points
-
-        data = input()
 
     output(my_dict)
 

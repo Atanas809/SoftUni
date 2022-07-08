ac36e37f9cfa0315f0176b5bfa07168fb46c2cba
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-08 08:58:39 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -10,3 +10,5 @@ if team == "Argentina":
     team_is_valid = True
     if souvenirs == "flags":
         price += number_of_souvenirs * 3.25
+    elif souvenirs == "caps":
+        price += number_of_souvenirs * 7.20

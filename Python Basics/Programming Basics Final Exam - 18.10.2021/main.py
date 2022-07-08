53365e93ca974f72aa0004caa28ff1a9f9680dca
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-08 08:55:53 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -1,6 +1,1 @@
-umber:
-    print("Wrong number!")
-    print("Try another one:", end = " ")
-    number = int(input())
-if number == correct_number:
-    print("Congratulation Borislava you win your bf's penis!")
+

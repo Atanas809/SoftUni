9aaa161ebd4180886c4d78d90cda89ebbce6395e
Atanas809 <atanas2380@abv.bg>
2022-07-08 08:54:18 +0300

    Added new file

---
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -0,0 +1,9 @@
+correct_number = 23
+print("Enter number:", end = " ")
+number = int(input())
+while number != correct_number:
+    print("Wrong number!")
+    print("Try another one:", end = " ")
+    number = int(input())
+if number == correct_number:
+    print("Congratulation Borislava you win your bf's penis!")

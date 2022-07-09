291c3a9de542bc02a9e31d6558cc570a7078503d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-09 11:51:44 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -5,3 +5,4 @@ for location in range(1, number_of_locations + 1):
     final_average_yield = 0
     average_yield = float(input())
     number_of_days = int(input())
+    for days in range(1, number_of_days + 1):

ae92a59c6ae3a17de7a59c79cf6275fc1d8486f5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-10 10:50:07 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -4,3 +4,4 @@ discount = int(input())
 total_sum_percent = int(input())
 
 price_for_printing = (price_for_page * 899) + (price_for_cover * 2)
+total_sum_for_printing = (price_for_printing - (price_for_printing * (discount / 100))) + price_for_designer

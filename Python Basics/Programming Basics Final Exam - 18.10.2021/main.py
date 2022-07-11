aebc8ce2e3b767c095799253e840372a7a7fc332
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-11 11:04:24 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -7,6 +7,7 @@ average_astronauts_height = float(input())
 
 spaceship_volume = width * length * height
 room_volume = (average_astronauts_height + 0.4) * 4
+
 total_astronauts = floor(spaceship_volume / room_volume)
 
 if total_astronauts < 3:

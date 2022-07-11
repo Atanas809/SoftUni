e92a8d1ef7b0361970797b7ed11b3b1df6ebeec8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-11 11:02:57 +0300

    Update main.py

--- Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
+++ Python Basics/Programming Basics Final Exam - 18.10.2021/main.py
@@ -5,3 +5,4 @@ length = float(input())
 height = float(input())
 average_astronauts_height = float(input())
 spaceship_volume = width * length * height
+room_volume = (average_astronauts_height + 0.4) * 4

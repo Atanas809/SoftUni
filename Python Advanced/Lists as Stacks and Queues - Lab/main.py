dc9a1ea6cf6c620bab3a37d975e308dddadad5d3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-13 10:59:46 +0300

    Update main.py

--- Python Advanced/Lists as Stacks and Queues - Lab/main.py
+++ Python Advanced/Lists as Stacks and Queues - Lab/main.py
@@ -1,7 +1,4 @@
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')

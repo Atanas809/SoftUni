e76333fa026e89bfc5c8593529fa00603351146a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-22 11:28:15 +0300

    Update key_revolver.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/key_revolver.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/key_revolver.py
@@ -2,3 +2,5 @@ from collections import deque
 
 bullet_price = int(input())
 size_barrel = int(input())
+bullets = [int(x) for x in input().split()]
+locks = deque([int(x) for x in input().split()])

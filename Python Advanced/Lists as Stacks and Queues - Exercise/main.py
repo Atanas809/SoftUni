541913e0346abfbd1ca61b69b759af9978579782
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-22 11:27:07 +0300

    Update main.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/main.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/main.py
@@ -1,14 +1,4 @@
 
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
-
         current_name = names(data)
         current_points = points(data)
 

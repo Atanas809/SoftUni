f75233c74c0784a3d77ca06ce30d4d21b2e3f081
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-24 11:01:10 +0300

    Update main.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/main.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/main.py
@@ -11,3 +11,13 @@ def robots():
 
     for r in data:
         current_robots.append(r)
+
+    return current_robots
+
+
+def seconds():
+    info = list(map(int, input().split(":")))
+
+    current_time = (info[0] * 3600) + (info[1] * 60) + info[2]
+
+    return current_time

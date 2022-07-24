efefcbbc0c851852e5cd564b2be8230d7c6c227d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-24 11:00:22 +0300

    Update main.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/main.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/main.py
@@ -1,16 +1,4 @@
 
-    matches = re.findall(letters, data)
-
-    current_name = ''.join(matches)
-
-    return current_name
-
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
     data = input()
 
     while data != "end of race":

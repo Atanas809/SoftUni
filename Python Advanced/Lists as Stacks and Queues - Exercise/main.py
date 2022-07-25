7e68e6162586a876c1bc704498f59b7402992e43
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-25 11:25:39 +0300

    Update main.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/main.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/main.py
@@ -1,14 +1,1 @@
 
-
-            if current_name == name.strip():
-                if current_name not in my_dict.keys():
-                    my_dict[current_name] = current_points
-                else:
-                    my_dict[current_name] += current_points
-
-        data = input()
-
-    output(my_dict)
-
-race()
-

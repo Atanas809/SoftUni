0f6f716434d78a4f47d6c4b13553627669b502f7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-26 10:20:05 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Lab/main.py
+++ Python Advanced/Tuples and Sets - Lab/main.py
@@ -1,8 +1,4 @@
 
-        current_name = names(data)
-        current_points = points(data)
-
-        for name in participants:
 
             if current_name == name.strip():
                 if current_name not in my_dict.keys():

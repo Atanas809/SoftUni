d1711cf18df0572e0f5d98a32d4a9d6f4972cf77
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-28 11:14:17 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Lab/main.py
+++ Python Advanced/Tuples and Sets - Lab/main.py
@@ -9,3 +9,5 @@ iterations = 0
 for n1 in range(0, len(numbers)):
     for n2 in range(n1 + 1, len(numbers)):
         if numbers[n1] + numbers[n2] == target:
+            print(f"{numbers[n1]} + {numbers[n2]} = {target}")
+            unique.add(f"{(numbers[n1], numbers[n2])}")

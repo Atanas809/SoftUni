e4b250a538e5bd5f6bb7232d7484325cf0285cbc
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-30 11:04:04 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Lab/main.py
+++ Python Advanced/Tuples and Sets - Lab/main.py
@@ -1,15 +1,4 @@
 
-    return current_name
-
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
 
         current_name = names(data)
         current_points = points(data)

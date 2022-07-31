aa3b034961963dd6880f21656cfe8c72ce15ef2d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-31 10:47:18 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Lab/main.py
+++ Python Advanced/Tuples and Sets - Lab/main.py
@@ -1,13 +1,5 @@
 
 
-    return current_points
-
-def names(data):
-
-    letters = r"[a-zA-Z]+"
-
-    matches = re.findall(letters, data)
-
     current_name = ''.join(matches)
 
     return current_name

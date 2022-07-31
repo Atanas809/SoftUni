4ade60b75142dcb03a7d862a8fd4353b778c9ecb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-07-31 10:48:54 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Lab/main.py
+++ Python Advanced/Tuples and Sets - Lab/main.py
@@ -6,3 +6,4 @@ def output(statistics):
     for name, grades in statistics.items():
         average_grade = avg(grades)
         string_grades = [str(f"{g:.2f}") for g in grades]
+        print(f"{name} -> {' '.join(string_grades)} (avg: {average_grade:.2f})")

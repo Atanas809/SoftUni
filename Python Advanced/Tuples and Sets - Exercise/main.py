9d9728342b026739399b46e3f44451d94f78da4b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-03 10:45:59 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Exercise/main.py
+++ Python Advanced/Tuples and Sets - Exercise/main.py
@@ -4,3 +4,6 @@ def result(even, odd):
 
     if even_sum == odd_sum:
         union_set = even.union(odd)
+        print(*union_set, sep=", ")
+    elif odd_sum > even_sum:
+        difference_set = odd.difference(even)

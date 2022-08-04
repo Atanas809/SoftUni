72285e3913d7422907aa40f6d2cb3531a524e746
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-04 10:47:47 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Exercise/main.py
+++ Python Advanced/Tuples and Sets - Exercise/main.py
@@ -2,3 +2,7 @@ def result(longest_intersection):
     output = [str(i) for i in longest_intersection]
 
     print(f"Longest intersection is [{', '.join(output)}] with length {len(longest_intersection)}")
+
+
+def intersections():
+    counter = int(input())

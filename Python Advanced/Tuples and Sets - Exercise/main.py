8c82a3cc9c3d286cfe8a3e6973175c6fba4143de
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-05 10:35:52 +0300

    Update main.py

--- Python Advanced/Tuples and Sets - Exercise/main.py
+++ Python Advanced/Tuples and Sets - Exercise/main.py
@@ -2,3 +2,5 @@ counters = [int(x) for x in input().split()]
 
 first_set = set(int(input()) for _ in range(counters[0]))
 second_set = set(int(input()) for _ in range(counters[1]))
+
+result = first_set.intersection(second_set)

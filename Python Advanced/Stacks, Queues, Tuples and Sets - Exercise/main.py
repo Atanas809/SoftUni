733bb46621c59087bd40622723ec3eb42036e081
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-08 11:49:04 +0300

    Update main.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
@@ -15,3 +15,4 @@ for _ in range(counter):
     elif command[0] == "Remove":
         current_numbers = set([int(x) for x in command[2:]])
         if command[1] == "First":
+            first = first.difference(current_numbers)

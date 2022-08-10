5af0274cedbac7cb2ae4309a1a27ed12b12ae7f3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-10 11:13:11 +0300

    Update main.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
@@ -4,3 +4,6 @@ chocolates = [int(x) for x in input().split(", ")]
 cups_of_milk = deque([int(x) for x in input().split(", ")])
 
 milkshakes = 0
+
+while chocolates and cups_of_milk and milkshakes < 5:
+    current_chocolate = chocolates.pop()

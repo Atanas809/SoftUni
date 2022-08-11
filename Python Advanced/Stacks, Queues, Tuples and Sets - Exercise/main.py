d1b2f1676d719ce765dd0ee8f9e838630a1d2c8a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-11 11:01:46 +0300

    Update main.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
@@ -31,3 +31,8 @@ while len(data) > 1:
 
     data.insert(len(data) // 2, first)
     data.insert(len(data) // 2, second)
+
+for key, value in secondary_colors.items():
+    if key in crafted_colors:
+        if value[0] not in crafted_colors or value[1] not in crafted_colors:
+            crafted_colors.remove(key)

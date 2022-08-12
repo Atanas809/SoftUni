c95f5cb9d6cd8df8b32f06f1020de8830617aa9c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-12 11:18:52 +0300

    Update main.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/main.py
@@ -2,3 +2,10 @@ from collections import deque
 
 materials = [int(x) for x in input().split()]
 magic_level = deque([int(x) for x in input().split()])
+
+presents = {
+    150: "Doll",
+    250: "Wooden train",
+    300: "Teddy bear",
+    400: "Bicycle"
+}

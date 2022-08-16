0701664e61544c1e29e6e429ac134d55ab8290ac
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-16 12:12:05 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Lab/main.py
+++ Python Advanced/Multidimensional Lists - Lab/main.py
@@ -5,3 +5,4 @@ def is_biggest(matrix, row, column):
         for m in range(column - 1):
             current_result = matrix[n][m] + matrix[n][m + 1] + matrix[n + 1][m] + matrix[n + 1][m + 1]
             if current_result > sum(result):
+                result[0] = matrix[n][m]

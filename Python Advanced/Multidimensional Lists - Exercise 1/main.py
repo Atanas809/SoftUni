7a8b95fa0766eccf776f1f4646ce5a80eaa73699
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-22 16:52:57 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -24,3 +24,6 @@ def valid_bombs(matrix, row, column, size):
     possible_bombs = [
         [row - 1, column - 1],
         [row - 1, column],
+        [row - 1, column + 1],
+        [row, column - 1],
+        [row, column + 1],

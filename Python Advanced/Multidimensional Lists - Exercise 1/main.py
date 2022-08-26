fb06c5e4fc86974a01b22bf42db9b7976406cc50
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-26 11:20:25 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -1,15 +1,5 @@
 
 
-    for x in range(0, len(best_points)):
-        current_name = [k for k, v in my_dict.items() if v == best_points[x]]
-
-        best_results.append(current_name[0])
-
-    print(f"""1st place: {best_results[0]}
-2nd place: {best_results[1]}
-3rd place: {best_results[2]}
-""")
-
 def points(data):
 
     digits = r"\d"

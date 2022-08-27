770873bd58c3eb15e1db8a8dc56901f28e1ba6de
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-27 10:29:44 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -1,14 +1,4 @@
-import re
-
-def output(my_dict):
-
-    val_list = list(sorted(my_dict.values(), reverse=True))
-
-    best_points = val_list[:3]
-
-    best_results = list()
-
-    for x in range(0, len(best_points)):
+s)):
         current_name = [k for k, v in my_dict.items() if v == best_points[x]]
 
         best_results.append(current_name[0])

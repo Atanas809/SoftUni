b5dd2933922fa563a7daa31217eb27d04056d7b5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-28 10:48:50 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -23,3 +23,6 @@ def move_is_valid(miner_row, miner_column, size, command):
         "down": [miner_row + 1, miner_column],
         "left": [miner_row, miner_column - 1],
         "right": [miner_row, miner_column + 1]
+    }
+
+    valid_moves = {}

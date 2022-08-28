941ed5322b65f929643ebd62a8f54c5d082f0b14
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-28 10:45:03 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -1,16 +1,5 @@
 
 
-def points(data):
-
-    digits = r"\d"
-
-    matches = re.findall(digits, data)
-
-    current_points = 0
-
-    for point in matches:
-        current_points += int(point)
-
     return current_points
 
 def names(data):

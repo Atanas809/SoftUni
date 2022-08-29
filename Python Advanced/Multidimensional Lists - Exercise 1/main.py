56c22711b94dc056a4fcf55c3fa9e553e00a70eb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-29 09:53:50 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -42,3 +42,8 @@ def is_coal(game_map):
         if "c" in row:
             counter = [x for x in row if x == "c"]
             coal += len(counter)
+
+    return coal
+
+
+def miner_position(game_map):

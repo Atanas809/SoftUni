655843f649c95ec77f58a7c49ae0bf71e03b6d5b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-30 10:14:02 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 1/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/main.py
@@ -1,12 +1,4 @@
 
-    data = input()
-
-    while data != "end of race":
-
-        current_name = names(data)
-        current_points = points(data)
-
-        for name in participants:
 
             if current_name == name.strip():
                 if current_name not in my_dict.keys():

51d6faa3085d36256076c3f6a9fe76c72cf1fc11
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-08-31 09:56:35 +0300

    Update radioactive_mutate_vampire_bunnies.py

--- Python Advanced/Multidimensional Lists - Exercise 1/radioactive_mutate_vampire_bunnies.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/radioactive_mutate_vampire_bunnies.py
@@ -55,3 +55,6 @@ def bunny_spreading(matrix, bunnies, dead):
             elif matrix[bunny_row][bunny_col - 1] != "B":
                 matrix[bunny_row][bunny_col - 1] = "B"
                 new_bunnies.add((bunny_row, bunny_col - 1))
+        if 0 <= bunny_row < len(matrix) and 0 <= bunny_col + 1 < len(matrix[0]):
+            if matrix[bunny_row][bunny_col + 1] == "P":
+                dead = True

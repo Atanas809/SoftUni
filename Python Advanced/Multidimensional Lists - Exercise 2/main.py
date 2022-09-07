5980116b7e0304c8c327741023f16a3925a07d9d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-07 10:16:43 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 2/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 2/main.py
@@ -3,3 +3,6 @@ def houses_around(next_row, next_col, matrix, presents_left, kids):
     possible_moves = [
         [next_row - 1, next_col],
         [next_row + 1, next_col],
+        [next_row, next_col - 1],
+        [next_row, next_col + 1],
+    ]

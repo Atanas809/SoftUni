57cd7c852daedd7252977c8a4a2823659f3f572b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-11 11:20:07 +0300

    Update main.py

--- Python Advanced/Multidimensional Lists - Exercise 2/main.py
+++ Python Advanced/Multidimensional Lists - Exercise 2/main.py
@@ -16,3 +16,8 @@ def moves(row, col, size):
         [row - 2, col + 1],
         [row + 2, col + 1],
         [row + 2, col - 1],
+        [row - 1, col - 2],
+        [row - 1, col + 2],
+        [row + 1, col - 2],
+        [row + 1, col + 2]
+    ]

d6889a3ae18ea425a385102f510117804fbf3454
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-12 11:53:42 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -1,1 +1,10 @@
+def sorting_cheeses(**kwargs):
+    cheeses = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
 
+    result = []
+
+    for key, value in cheeses:
+        result.append(key)
+        result.extend(sorted(value, reverse=True))
+
+    return '\n'.join([str(x) for x in result])

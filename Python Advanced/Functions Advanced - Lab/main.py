8923f0fc54ac8e63d36f48bbd3a3005a16ef0313
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-12 11:54:28 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -13,3 +13,8 @@ def sorting_cheeses(**kwargs):
 # Test inputs:
 print(
     sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)

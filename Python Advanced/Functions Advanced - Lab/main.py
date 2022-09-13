5944ce115bfa45f7884544140b0909452b860871
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-13 10:53:07 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -1,11 +1,4 @@
-
-        for name in participants:
-
-            if current_name == name.strip():
-                if current_name not in my_dict.keys():
-                    my_dict[current_name] = current_points
-                else:
-                    my_dict[current_name] += current_points
+_points
 
         data = input()
 

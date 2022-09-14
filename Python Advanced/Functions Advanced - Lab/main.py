5eebd3234d2c856523b895dac7898b89fe7f0dc2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-14 10:55:13 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -1,3 +1,10 @@
 def operate(operator, *args):
     def add():
         return sum(args)
+
+    def subtract():
+        result = args[0]
+        for x in args[1:]:
+            result -= x
+
+        return result

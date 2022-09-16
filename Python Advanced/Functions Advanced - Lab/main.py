79a9cc4883687afd82453b3783f836d207dd432b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-16 11:24:00 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -1,12 +1,5 @@
 
 
-    for point in matches:
-        current_points += int(point)
-
-    return current_points
-
-def names(data):
-
     letters = r"[a-zA-Z]+"
 
     matches = re.findall(letters, data)

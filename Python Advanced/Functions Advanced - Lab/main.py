d354862d138a30d369667c025b59468caf22cd6d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-17 09:59:39 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Lab/main.py
+++ Python Advanced/Functions Advanced - Lab/main.py
@@ -1,8 +1,12 @@
 def recursive_power(number, power):
-
     if power == 0:
         return 1
 
     result = number * recursive_power(number, power - 1)
 
     return result
+
+
+# Test inputs:
+print(recursive_power(2, 10))
+print(recursive_power(10, 100))

bbef09985a1dade06c68aa17a64adc476c3bdbd0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-18 11:05:52 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -5,3 +5,5 @@ def age_assignment(*args, **kwargs):
         start_with = name[0]
         age = kwargs[start_with]
         result.append(f"{name} is {age} years old.")
+
+    return '\n'.join(sorted(result))

209794a4afeea8b270933518db45f681ecf344dc
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-18 11:03:24 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -1,10 +1,5 @@
 
 
-    while data != "end of race":
-
-        current_name = names(data)
-        current_points = points(data)
-
         for name in participants:
 
             if current_name == name.strip():

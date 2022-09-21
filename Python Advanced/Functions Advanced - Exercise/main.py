d03232b6136aaa496838541110e5ff8d764bedcb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-21 11:07:12 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -13,3 +13,21 @@ def fill_the_box(height, length, width, *args):
 
         if current_cube == "Finish":
             break
+
+        if box_volume >= current_cube:
+            box_volume -= current_cube
+
+        else:
+            current_cube -= box_volume
+            box_volume = 0
+            cubes_left += current_cube
+
+    if cubes_left:
+        return f"No more free space! You have {cubes_left} more cubes."
+    else:
+        return f"There is free space in the box. You could put {box_volume} more cubes."
+
+
+# Test inputs:
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))

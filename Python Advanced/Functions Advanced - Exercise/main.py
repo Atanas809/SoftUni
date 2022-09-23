a381802260eae66092deb608909a6190ff9b5895
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-23 10:21:34 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -2,3 +2,5 @@ def grocery_store(**kwargs):
     sorted_dict = dict(sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0])))
 
     result = [f"{key}: {value}" for key, value in sorted_dict.items()]
+
+    return '\n'.join(result)

46e7ab4af146c32b2d9dbfdb82f708652992f95b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-26 11:33:23 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -37,3 +37,8 @@ def math_operations(*args, **kwargs):
     result = [f"{key}: {value:.1f}" for key, value in sorted_dict.items()]
 
     return '\n'.join(result)
+
+
+# Test inputs:
+print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+print(math_operations(-1.0, 0.5, 1.6, 0.5, 6.1, -2.8, 80.0, a=0, s=(-2.3), d=0, m=0))

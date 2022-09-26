4ee77876c63b4bc372115c8e4809f0f9b20fd8fa
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-09-26 11:31:04 +0300

    Update main.py

--- Python Advanced/Functions Advanced - Exercise/main.py
+++ Python Advanced/Functions Advanced - Exercise/main.py
@@ -4,7 +4,9 @@ from collections import deque
 def math_operations(*args, **kwargs):
 
     values = deque(args)
-
     index = 1
 
     while values:
+        if index > 4:
+            index = 1
+            continue

bf90c2862a979cb63646efe2f21f7328b4171b59
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-06 12:52:49 +0300

    Update main.py

--- Python Advanced/Error Handling - Exercise/main.py
+++ Python Advanced/Error Handling - Exercise/main.py
@@ -41,3 +41,9 @@ print(numbers_dictionary)
 numbers_dictionary = {}
 line = input()
 
+
+while line != "Search":
+    number_as_string = line
+    try:
+        number = int(input())
+        numbers_dictionary[number_as_string] = number

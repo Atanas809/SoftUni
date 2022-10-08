4258cd84016b346b24d0bc6809bd5557f804c98c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-08 11:08:19 +0300

    Update main.py

--- Python Advanced/File Handling - Lab/main.py
+++ Python Advanced/File Handling - Lab/main.py
@@ -1,10 +1,4 @@
 
-    best_points = val_list[:3]
-
-    best_results = list()
-
-    for x in range(0, len(best_points)):
-        current_name = [k for k, v in my_dict.items() if v == best_points[x]]
 
         best_results.append(current_name[0])
 

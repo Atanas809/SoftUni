5ac4bf30d8d644367ea566dd2518ba1a8e519b45
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-12 10:40:38 +0300

    Update main.py

--- Python Advanced/File Handling - Lab/main.py
+++ Python Advanced/File Handling - Lab/main.py
@@ -11,3 +11,7 @@ occurrences = dict()
 with open("./input.txt", "r") as file:
     for line in file:
         current_words = re.findall(r"\b\S+\b", line.lower())
+        for word in searched_words:
+            if word in current_words:
+                counter = current_words.count(word)
+                if word not in occurrences.keys():

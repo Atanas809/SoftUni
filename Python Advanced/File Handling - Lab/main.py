f6a43db799485379f71e91123b313dc07baf7078
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-12 10:40:27 +0300

    Update main.py

--- Python Advanced/File Handling - Lab/main.py
+++ Python Advanced/File Handling - Lab/main.py
@@ -5,3 +5,9 @@ searched_words = ""
 with open("./words.txt", "r") as words:
     for word in words:
         searched_words = word.split()
+
+occurrences = dict()
+
+with open("./input.txt", "r") as file:
+    for line in file:
+        current_words = re.findall(r"\b\S+\b", line.lower())

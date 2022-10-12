ff2a8586244e30be9c2f72d57dcbd25e741b5fe0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-12 10:39:33 +0300

    Update main.py

--- Python Advanced/File Handling - Lab/main.py
+++ Python Advanced/File Handling - Lab/main.py
@@ -1,12 +1,5 @@
 
 
-    data = input()
-
-    while data != "end of race":
-
-        current_name = names(data)
-        current_points = points(data)
-
         for name in participants:
 
             if current_name == name.strip():

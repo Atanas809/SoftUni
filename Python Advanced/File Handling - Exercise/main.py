76b889bd6e3f02994672a13908fdc693b91ad5c1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-13 10:25:46 +0300

    Update main.py

--- Python Advanced/File Handling - Exercise/main.py
+++ Python Advanced/File Handling - Exercise/main.py
@@ -2,7 +2,11 @@ import os
 
 
 def sorted_result(dictionary):
-
     sorting = sorted(dictionary.items(), key=lambda x: (x[0], x[1]))
-
     return sorting
+
+
+path = "./"
+result = dict()
+
+for _, _, files in os.walk(path):

9418a39eb213e10dd7717150f7ff49d2cfa0f98e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-14 10:18:40 +0300

    Update main.py

--- Python Advanced/File Handling - Exercise/main.py
+++ Python Advanced/File Handling - Exercise/main.py
@@ -2,3 +2,5 @@ def output(current_line):
     symbols_to_replace = {"-", ",", ".", "!", "?"}
 
     for symbol in symbols_to_replace:
+        if symbol in current_line:
+            current_line = current_line.replace(symbol, "@")

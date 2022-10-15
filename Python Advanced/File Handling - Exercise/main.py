584ac89015b1ff87741094b3d9ffa18d08acc5c7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-15 10:54:04 +0300

    Update main.py

--- Python Advanced/File Handling - Exercise/main.py
+++ Python Advanced/File Handling - Exercise/main.py
@@ -1,7 +1,4 @@
-
-
-            if current_name == name.strip():
-                if current_name not in my_dict.keys():
+:
                     my_dict[current_name] = current_points
                 else:
                     my_dict[current_name] += current_points

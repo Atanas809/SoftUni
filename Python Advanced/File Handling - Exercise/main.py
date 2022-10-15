93051d61e663b8a68908656dfa5e5fb0e96cdc12
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-15 10:54:33 +0300

    Update main.py

--- Python Advanced/File Handling - Exercise/main.py
+++ Python Advanced/File Handling - Exercise/main.py
@@ -1,4 +1,18 @@
+from os.path import exists
+from os import remove
 
-
-race()
-
+# Test input:
+"""
+Create-file.txt
+Add-file.txt-First Line
+Add-file.txt-Second Line
+Create-file.txt
+Add-file.txt-First Line
+Add-file.txt-Second Line
+Replace-random.txt-Some-some
+Replace-file.txt-First-1st
+Replace-file.txt-Second-2nd
+Delete-random.txt
+Delete-file.txt
+End
+"""

b776e7623cc6b56c6a0c21511f31efd541d0665d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-16 14:27:43 +0300

    Update main.py

--- Python Advanced/File Handling - Exercise/main.py
+++ Python Advanced/File Handling - Exercise/main.py
@@ -6,3 +6,7 @@ def symbols_counter(current_line):
     punctuations = string.punctuation
 
     counter = 0
+
+    for symbol in punctuations:
+        if symbol in current_line:
+            counter += current_line.count(symbol)

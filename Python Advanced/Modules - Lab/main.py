2424caa9c3889ce093ee24bf9eba02d2e13339b9
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-17 11:35:23 +0300

    Update main.py

--- Python Advanced/Modules - Lab/main.py
+++ Python Advanced/Modules - Lab/main.py
@@ -1,12 +1,4 @@
-import re
-
-def output(my_dict):
-
-    val_list = list(sorted(my_dict.values(), reverse=True))
-
-    best_points = val_list[:3]
-
-    best_results = list()
+st()
 
     for x in range(0, len(best_points)):
         current_name = [k for k, v in my_dict.items() if v == best_points[x]]

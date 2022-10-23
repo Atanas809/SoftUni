7e089286a6c0ec5c671646ebb5a422e824d9dcef
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-23 11:37:05 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,12 @@ from collections import deque
 105 20 30 25
 120 60 11 400 10 1
 """
+
+
+def other_operations(material, magic, items, result):
+    if result < 100:
+        if result % 2 == 0:
+            material *= 2
+            magic *= 3
+            final_result = material + magic
+            valid_values(final_result, items)

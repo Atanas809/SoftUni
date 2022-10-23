63a58f5e84ee0d68a47a0dc7d22928147e9f64b7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-23 11:36:28 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -1,15 +1,5 @@
 
 
-def race():
-
-    participants = input().split(",")
-
-    my_dict = dict()
-
-    data = input()
-
-    while data != "end of race":
-
         current_name = names(data)
         current_points = points(data)
 

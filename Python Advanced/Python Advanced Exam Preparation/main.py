7dc7b833985d21d2d26f6b71b23b3586f1e6ccea
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-25 10:21:03 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,10 @@ from collections import deque
 5, 25, 25, 115
 5, 15, 25, 35
 """
+
+
+def possible_bomb_craft(result):
+    if result == 40 or result == 60 or result == 120:
+        return True
+
+    return False

32758665e56cf0238c14467b9fb73ee6487d8359
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-26 10:33:32 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -14,3 +14,5 @@ Q . . . Q . . .
 def get_board_values(valid_moves, board):
     result = []
     for key, value in valid_moves.items():
+        current_count = {f"{key}": 0}
+        for move in value:

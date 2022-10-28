57fe7d326c42a92780dde5cbbe3fe9a03d9bbbeb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-28 10:52:21 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -34,3 +34,12 @@ def stock_availability(*args):
         products_list.popleft()
 
     return list(products_list)
+
+
+print(stock_availability(["choco", "vanilla", "banana"], "delivery", "caramel", "berry"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "delivery", "cookie", "banana"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell", 3))
+print(stock_availability(["chocolate", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(["cookie", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell", "cookie"))

d92193cc35483b85b412235772c057f6f9bb48bd
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-28 10:51:25 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -13,3 +13,7 @@ def stock_availability(*args):
     products = args[0]
     command = args[1]
     products_list = deque(products)
+
+    if len(args) > 2:
+        if command == "delivery":
+            items_to_be_delivered = args[2:]

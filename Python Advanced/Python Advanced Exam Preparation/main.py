cc47e239e9a606202227b283f43076142f4552b7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-29 11:04:58 +0300

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,10 @@ from collections import deque
 5, 6, 4, 16, 11, 5, 30, 2, 3, 27
 1, 13, 5, 3, -7, 32, 19, 3, 5, 7, 22
 """
+
+
+def enough_fireworks(fireworks):
+    if fireworks["Palm firework"] >= 3 and fireworks["Willow firework"] >= 3 and fireworks["Crossette firework"] >= 3:
+        return True
+
+    return False

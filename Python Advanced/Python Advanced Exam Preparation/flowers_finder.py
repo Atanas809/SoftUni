419b4ab45b99566a6c0a0cb4c13e4a9ec712df9a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-30 08:44:54 +0200

    Update flowers_finder.py

--- Python Advanced/Python Advanced Exam Preparation/flowers_finder.py
+++ Python Advanced/Python Advanced Exam Preparation/flowers_finder.py
@@ -1,10 +1,5 @@
 
 
-if word_found:
-    print(f"Word found: {word_found}")
-else:
-    print("Cannot find any word!")
-
 if vowels:
     print(f"Vowels left: {' '.join([x for x in vowels])}")
 if consonants:

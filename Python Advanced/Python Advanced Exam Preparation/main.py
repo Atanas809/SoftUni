7b56681d0be0e74f39a31fcbfa7361278aba8285
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-31 10:49:23 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -10,3 +10,7 @@ def extra_parameters(command, end_or_beginning, numbers, my_list):
             my_list.append(num)
     elif command == "remove" and end_or_beginning == "beginning":
         for _ in range(numbers[0]):
+            my_list.popleft()
+    elif command == "remove" and end_or_beginning == "end":
+        for _ in range(numbers[0]):
+            my_list.pop()

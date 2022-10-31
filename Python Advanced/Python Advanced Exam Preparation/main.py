13f4dcf91db5ca6ebec3ee56e92c64f78aa9e30c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-31 10:48:48 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,5 @@ def extra_parameters(command, end_or_beginning, numbers, my_list):
     if command == "add" and end_or_beginning == "beginning":
         for num in numbers[::-1]:
             my_list.appendleft(num)
+    elif command == "add" and end_or_beginning == "end":
+        for num in numbers:

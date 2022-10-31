f2eea196e23db980b9c7923b56e89191374ccee7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-31 10:48:37 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -3,3 +3,5 @@ from collections import deque
 
 def extra_parameters(command, end_or_beginning, numbers, my_list):
     if command == "add" and end_or_beginning == "beginning":
+        for num in numbers[::-1]:
+            my_list.appendleft(num)

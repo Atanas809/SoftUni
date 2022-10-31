dbd533addfd30967aecac65c45206fa9afaafbb2
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-10-31 10:50:49 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -35,3 +35,8 @@ def list_manipulator(*args):
 print(list_manipulator([1, 2, 3], "remove", "end"))
 print(list_manipulator([1, 2, 3], "remove", "beginning"))
 print(list_manipulator([1, 2, 3], "add", "beginning", 20))
+print(list_manipulator([1, 2, 3], "add", "end", 30))
+print(list_manipulator([1, 2, 3], "remove", "end", 2))
+print(list_manipulator([1, 2, 3], "remove", "beginning", 2))
+print(list_manipulator([1, 2, 3], "add", "beginning", 20, 30, 40))
+print(list_manipulator([1, 2, 3], "add", "end", 30, 40, 50))

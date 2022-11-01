f92dec28f4459f553111d85bd4d381baf6051de0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-01 10:20:48 +0200

    Update list_pureness.py

--- Python Advanced/Python Advanced Exam Preparation/list_pureness.py
+++ Python Advanced/Python Advanced Exam Preparation/list_pureness.py
@@ -5,3 +5,6 @@ def best_list_pureness(*args):
     numbers = deque(args[0])
     counter = args[-1]
     best_pureness = float("-inf")
+    best_index = 0
+    for x in range(counter):
+        current_pureness = 0

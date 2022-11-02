8cfb0b84a1545c2f221e0821c951c4aac21b70e0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-02 10:17:05 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -1,1 +1,9 @@
-
+def element_on_position(next_row, next_col, field):
+    if field[next_row][next_col] == "R":
+        return "R"
+    elif field[next_row][next_col] == "W":
+        return "W"
+    elif field[next_row][next_col] == "M":
+        return "M"
+    elif field[next_row][next_col] == "C":
+        return "C"

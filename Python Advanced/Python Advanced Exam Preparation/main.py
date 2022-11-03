798217814b539463eb1015ef20026fc87ad25c4f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-03 10:15:56 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -27,3 +27,7 @@ b - - - - - - -
 def capture(black_row, black_col, white_row, white_col):
     if abs(black_row - white_row) == 1:
         return ("w", black_row, black_col)
+    else:
+        if abs(black_row - white_row) % 2 == 0:
+            while True:
+                white_row -= 1

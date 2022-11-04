e32cc8e381691abe2a2f8b466af11d487ee7cf2d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-04 09:40:40 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -4,3 +4,5 @@ bowls_of_ramen = [int(x) for x in input().split(", ")]
 customers = deque([int(x) for x in input().split(", ")])
 
 while bowls_of_ramen and customers:
+    current_bowl = bowls_of_ramen.pop()
+    current_customer = customers.popleft()

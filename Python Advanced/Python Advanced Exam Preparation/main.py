36d58102c79aa57a19e36b20c20ba359f1c70fd1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-04 09:40:30 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -2,3 +2,5 @@ from collections import deque
 
 bowls_of_ramen = [int(x) for x in input().split(", ")]
 customers = deque([int(x) for x in input().split(", ")])
+
+while bowls_of_ramen and customers:

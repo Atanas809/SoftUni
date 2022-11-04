a710c1d04efc14fe09b6d41152da2b6a10df8317
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-04 09:40:52 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -6,3 +6,6 @@ customers = deque([int(x) for x in input().split(", ")])
 while bowls_of_ramen and customers:
     current_bowl = bowls_of_ramen.pop()
     current_customer = customers.popleft()
+
+    if current_bowl > current_customer:
+        current_bowl -= current_customer

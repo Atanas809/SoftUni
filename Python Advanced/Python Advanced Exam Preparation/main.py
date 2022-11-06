ec4704d6a0748543aad8a4107b0d7d73a8c7082c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-06 11:34:20 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -3,3 +3,10 @@ def shopping_list(budget, **kwargs):
         return "You do not have enough budget."
 
     shopping_bag = []
+
+    for key, value in kwargs.items():
+        product = key
+        price = value[0]
+        quantity = value[1]
+        if len(shopping_bag) == 5:
+            break

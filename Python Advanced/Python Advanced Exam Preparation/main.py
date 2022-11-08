03167269c20de4110b6e16df567fd70ec073513b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-08 10:15:34 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,5 @@ def start_spring(**kwargs):
         if value not in my_dict.keys():
             my_dict[value] = list()
         my_dict[value].append(key)
+
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda x: (-len(x[1]), x[0])))

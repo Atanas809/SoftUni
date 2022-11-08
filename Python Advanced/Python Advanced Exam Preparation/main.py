fc16ad3da6a176a616e3e99d19bc48b72bfdae33
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-08 10:15:22 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -1,2 +1,7 @@
 def start_spring(**kwargs):
     my_dict = dict()
+
+    for key, value in kwargs.items():
+        if value not in my_dict.keys():
+            my_dict[value] = list()
+        my_dict[value].append(key)

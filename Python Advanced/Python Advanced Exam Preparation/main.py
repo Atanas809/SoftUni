265251741b2a4b29154f8f8fbed66c2225b975ee
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-09 10:07:08 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -4,3 +4,6 @@ def words_sorting(*args):
         my_dict[word] = sum([ord(x) for x in word])
 
     result = sum(my_dict.values())
+
+    if result % 2 == 0:
+        sorted_by_keys = sorted(my_dict.items(), key=lambda x: x[0])

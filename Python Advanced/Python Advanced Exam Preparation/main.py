9b8b59a545f6311813eae902898cf0b64eea2020
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-23 10:35:19 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -5,3 +5,7 @@ paper_size = [int(x) for x in input().split(", ")]
 
 box_size = 50
 boxes_made = 0
+
+while egg_size and paper_size:
+    current_egg = egg_size.popleft()
+    current_paper = paper_size.pop()

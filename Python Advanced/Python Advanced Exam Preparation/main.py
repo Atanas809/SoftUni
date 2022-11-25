c87a14cd5883b1ab391285ec23763394a33e0dda
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-25 11:01:15 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -3,3 +3,13 @@ def next_player(current_player):
         return 0
 
     return 1
+
+
+def move():
+    command = input()
+    command = command.replace("(", "")
+    command = command.replace(")", "")
+    command = command.replace(",", " ")
+    row, col = [int(x) for x in command.split()]
+
+    return row, col

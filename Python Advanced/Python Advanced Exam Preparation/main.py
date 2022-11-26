2296785bcc1f75d8834f7a4816ef38bc7ed3044e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-26 11:25:34 +0200

    Update main.py

--- Python Advanced/Python Advanced Exam Preparation/main.py
+++ Python Advanced/Python Advanced Exam Preparation/main.py
@@ -1,1 +1,6 @@
+def is_valid(sorted_meals):
 
+    if len(sorted_meals["Pizza"]) == 0 and len(sorted_meals["Soup"]) == 0 and len(sorted_meals["Dessert"]) == 0:
+        return False
+
+    return True

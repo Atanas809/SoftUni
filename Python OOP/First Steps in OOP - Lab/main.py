a5e89af568a53faf654f07028f1ce6712eb7d70d
Atanas809 <atanas2380@abv.bg>
2022-11-30 10:07:41 +0200

    Added new file for oop

---
+++ Python OOP/First Steps in OOP - Lab/main.py
@@ -0,0 +1,66 @@
+# Current output:
+"""
+global
+outer: local
+inner: nonlocal
+outer: local
+global
+"""
+
+x = "global"
+
+
+def outer():
+    x = "local"
+
+    def inner():
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+print(x)
+outer()
+print(x)
+
+# Expected output:
+"""
+global
+outer: local
+inner: nonlocal
+outer: nonlocal
+global: changed!
+
+"""
+
+x = "global"
+
+
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        global x
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+print(x)
+outer()
+print(x)

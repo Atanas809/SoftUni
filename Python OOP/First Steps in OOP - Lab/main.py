f56d45b95aa912adeb8306c519f5d4bd525b9943
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-30 10:08:48 +0200

    Update main.py

--- Python OOP/First Steps in OOP - Lab/main.py
+++ Python OOP/First Steps in OOP - Lab/main.py
@@ -1,31 +1,5 @@
 
 
-    print("outer:", x)
-    inner()
-    print("outer:", x)
-    change_global()
-
-
-print(x)
-outer()
-print(x)
-
-# Expected output:
-"""
-global
-outer: local
-inner: nonlocal
-outer: nonlocal
-global: changed!
-
-"""
-
-x = "global"
-
-
-def outer():
-    x = "local"
-
     def inner():
         nonlocal x
         x = "nonlocal"

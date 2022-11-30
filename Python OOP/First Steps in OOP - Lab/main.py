d131384e6777e312e1e56b13002f8a5f2cc50bc5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-30 10:08:40 +0200

    Update main.py

--- Python OOP/First Steps in OOP - Lab/main.py
+++ Python OOP/First Steps in OOP - Lab/main.py
@@ -1,18 +1,5 @@
 
 
-x = "global"
-
-
-def outer():
-    x = "local"
-
-    def inner():
-        x = "nonlocal"
-        print("inner:", x)
-
-    def change_global():
-        x = "global: changed!"
-
     print("outer:", x)
     inner()
     print("outer:", x)

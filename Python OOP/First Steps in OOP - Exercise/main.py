ffae8267f053e51ecd40fbd17ce7fbd372f0770c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-03 11:26:24 +0200

    Update main.py

--- Python OOP/First Steps in OOP - Exercise/main.py
+++ Python OOP/First Steps in OOP - Exercise/main.py
@@ -2,3 +2,5 @@ class Employee:
     def __init__(self, id, first_name, last_name, salary):
         self.id = id
         self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary

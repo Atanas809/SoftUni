8ec0b7a1a6200119599b82469c3915538d5a487c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-04 10:40:32 +0200

    Update main.py

--- Python OOP/First Steps in OOP - Exercise/main.py
+++ Python OOP/First Steps in OOP - Exercise/main.py
@@ -19,3 +19,10 @@ class Flower:
 
 flower = Flower("Lilly", 100)
 flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())
+flower.water(30)
+print(flower.status())

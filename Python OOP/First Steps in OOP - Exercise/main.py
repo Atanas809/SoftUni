0070fd5c61207f1b4812ff3f0fea30e369bbbe0a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-07 11:39:50 +0200

    Update main.py

--- Python OOP/First Steps in OOP - Exercise/main.py
+++ Python OOP/First Steps in OOP - Exercise/main.py
@@ -23,3 +23,8 @@ class SteamUser:
 
 user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
 print(user.play("Fortnite", 3))
+print(user.play("Oxygen Not Included", 5))
+print(user.buy_game("CS:GO"))
+print(user.buy_game("Oxygen Not Included"))
+print(user.play("Oxygen Not Included", 6))
+print(user.status())

d7b359732f28783c15e126d08377956900cd3156
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-11 11:47:12 +0200

    Update main.py

--- Python OOP/Classes and Objects - Lab/main.py
+++ Python OOP/Classes and Objects - Lab/main.py
@@ -3,3 +3,11 @@ class Vehicle:
         self.mileage = mileage
         self.max_speed = max_speed
         self.gadgets = []
+
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)

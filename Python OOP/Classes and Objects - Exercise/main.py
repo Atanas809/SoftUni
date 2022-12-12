8ec27d3ef2e9a3122dcf4cec4f3ef7d7e201fed9
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-12 23:29:42 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -1,13 +1,4 @@
 
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
-library.get_book('J.K.Rowling', 'The Deathly Hallows', 17, user)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
 print(library.get_book('J.K.Rowling', 'The Deathly Hallows', 10, user))
 print(library.return_book('J.K.Rowling', 'The Cursed Child', user))
 library.return_book('J.K.Rowling', 'The Deathly Hallows', user)

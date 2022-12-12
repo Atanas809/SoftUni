75ff13e7ab7b3dd14179c78efd348b14bbb38295
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-12 23:29:14 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -1,12 +1,4 @@
 
-print(registration.change_username(2, 'Igor', library))
-print(registration.change_username(12, 'Peter', library))
-print(registration.change_username(12, 'George', library))
-
-[print(f'{user_record.user_id}, {user_record.username}, {user_record.books}') for user_record in library.user_records]
-
-
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
                                                 'The Prisoner of Azkaban',
                                                 'The Goblet of Fire',
                                                 'The Order of the Phoenix',

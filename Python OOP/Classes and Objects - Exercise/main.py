987c242666f6cb8eaa6c3136341e6446507e558c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-12 23:31:14 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -1,1 +1,6 @@
 class PizzaDelivery:
+    def __init__(self, name: str, price: float, ingredients: dict):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False

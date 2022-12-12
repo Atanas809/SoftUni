2ccee46af3998c07d3944f180098e0d35738fb31
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-12 23:32:01 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -10,3 +10,8 @@ class PizzaDelivery:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
         if ingredient not in self.ingredients.keys():
             self.ingredients[ingredient] = quantity
+
+        else:
+            self.ingredients[ingredient] += quantity
+
+        self.price += price_per_quantity * quantity

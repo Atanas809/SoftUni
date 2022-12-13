5709b43e4409440c59257d4b72406dccea80b718
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-13 08:19:14 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -1,6 +1,1 @@
-he Deathly Hallows', 10, user))
-print(library.return_book('J.K.Rowling', 'The Cursed Child', user))
-library.return_book('J.K.Rowling', 'The Deathly Hallows', user)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
+

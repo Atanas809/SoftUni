fa66c735d6b6cbf9a9d51aba75224bde23a2b204
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-13 08:20:18 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -2,3 +2,8 @@ class Time:
     max_hours = 23
     max_minutes = 59
     max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds

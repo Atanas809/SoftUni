ce80580009546330a16e956389754a19f8fc5221
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-14 08:25:38 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -13,3 +13,7 @@ class Vet:
         Vet.animals.append(animal_name)
         self.animals.append(animal_name)
         return f"{animal_name} registered in the clinic"
+
+    def unregister_animal(self, animal_name):
+        if animal_name not in self.animals:
+            return f"{animal_name} not in the clinic"

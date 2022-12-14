468e9a2411e0064c03dddb0927cfafe91e82f67e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-14 08:25:06 +0200

    Update main.py

--- Python OOP/Classes and Objects - Exercise/main.py
+++ Python OOP/Classes and Objects - Exercise/main.py
@@ -5,3 +5,7 @@ class Vet:
     def __init__(self, name):
         self.name = name
         self.animals = []
+
+    def register_animal(self, animal_name):
+        if len(Vet.animals) >= Vet.space:
+            return "Not enough space"

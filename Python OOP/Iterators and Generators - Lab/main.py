207c1a7a2cafe8ec91b6e5f2e266d030382980c1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-17 10:44:13 +0200

    Update main.py

--- Python OOP/Iterators and Generators - Lab/main.py
+++ Python OOP/Iterators and Generators - Lab/main.py
@@ -1,14 +1,4 @@
 
-
-
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
-library.get_book('J.K.Rowling', 'The Deathly Hallows', 17, user)
-print(library.books_available)
 print(library.rented_books)
 print(user.books)
 print(library.get_book('J.K.Rowling', 'The Deathly Hallows', 10, user))

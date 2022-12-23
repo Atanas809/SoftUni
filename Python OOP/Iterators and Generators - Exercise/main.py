eae237e3e575e1b54234255ba9ecb0064e7da006
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-23 12:06:53 +0200

    Update main.py

--- Python OOP/Iterators and Generators - Exercise/main.py
+++ Python OOP/Iterators and Generators - Exercise/main.py
@@ -2,3 +2,7 @@ def read_next(*args):
     for x in args:
         for char in x:
             yield char
+
+
+for item in read_next('string', (2,), {'d': 1, 'i': 2, 'c': 3, 't': 4}):
+    print(item, end='')

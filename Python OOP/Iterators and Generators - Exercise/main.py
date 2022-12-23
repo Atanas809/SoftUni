374bdc8b8115baa250a76bd98929bff2f2af6b8f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-23 12:09:13 +0200

    Update main.py

--- Python OOP/Iterators and Generators - Exercise/main.py
+++ Python OOP/Iterators and Generators - Exercise/main.py
@@ -1,3 +1,9 @@
 class sequence_repeat:
     def __init__(self, text, number):
         self.text = text
+        self.number = number
+        self.start = -1
+        self.index = 0
+
+    def __iter__(self):
+        return self

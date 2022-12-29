25af82b82cdf1723ac8ec69ccc9501a761adc191
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-29 12:35:29 +0200

    Update stack_of_strings.py

--- Python OOP/Inheritance - Lab/single_inheritance/stack_of_strings.py
+++ Python OOP/Inheritance - Lab/single_inheritance/stack_of_strings.py
@@ -1,16 +1,1 @@
 
-
-    def __str__(self):
-        reversed_list = reversed(self.data)
-        return f"[{', '.join(x for x in reversed_list)}]"
-
-
-s = Stack()
-s.push("5")
-s.push("3")
-s.push("2")
-print(s)
-print(s.pop())
-print(s)
-print(s.top())
-print(s.is_empty())

ada257ce4207a73afd3739da496f2d4da851dd69
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-12-29 12:35:14 +0200

    Update stack_of_strings.py

--- Python OOP/Inheritance - Lab/single_inheritance/stack_of_strings.py
+++ Python OOP/Inheritance - Lab/single_inheritance/stack_of_strings.py
@@ -1,12 +1,4 @@
-class Stack:
-    def __init__(self):
-        self.data = []
 
-    def push(self, element):
-        if not isinstance(element, str):
-            raise TypeError("Only string types are allowed!")
-
-        self.data.append(element)
 
     def pop(self):
         return self.data.pop()

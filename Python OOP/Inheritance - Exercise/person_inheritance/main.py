9ae26ac1b4662f8f7286543f3976dbfcef11b2c8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-01 23:12:08 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/person_inheritance/main.py
+++ Python OOP/Inheritance - Exercise/person_inheritance/main.py
@@ -1,8 +1,1 @@
-from person_inheritance.child import Child
-from person_inheritance.person import Person
 
-person = Person("Peter", 25)
-child = Child("Peter Junior", 5)
-print(person.name)
-print(person.age)
-print(child.__class__.__bases__[0].__name__)

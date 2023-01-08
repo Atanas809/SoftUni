a1716a91c93c5edb6df4e781dfcdeecf68a93657
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-08 12:30:43 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/need_for_speed/main.py
+++ Python OOP/Inheritance - Exercise/need_for_speed/main.py
@@ -1,8 +1,1 @@
 
-print(vehicle.fuel)
-family_car = FamilyCar(150, 150)
-family_car.drive(50)
-print(family_car.fuel)
-family_car.drive(50)
-print(family_car.fuel)
-print(family_car.__class__.__bases__[0].__name__)

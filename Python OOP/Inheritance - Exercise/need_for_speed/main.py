d813e147a1221a939bcbdf1b8da6472d9631f717
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-12 20:30:00 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/need_for_speed/main.py
+++ Python OOP/Inheritance - Exercise/need_for_speed/main.py
@@ -5,3 +5,9 @@ class Vehicle:
         self.fuel = fuel
         self.horse_power = horse_power
         self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        needed_fuel = self.fuel_consumption * kilometers
+
+        if self.fuel >= needed_fuel:
+            self.fuel -= needed_fuel

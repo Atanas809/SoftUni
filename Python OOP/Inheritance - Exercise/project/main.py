f17286dfb9d135b8947b271ae5bee9b0a1869ff3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-19 21:35:30 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/project/main.py
+++ Python OOP/Inheritance - Exercise/project/main.py
@@ -1,10 +1,4 @@
-from drink import Drink
-from food import Food
-from product_repository import ProductRepository
 
-food = Food("apple")
-drink = Drink("water")
-repo = ProductRepository()
 repo.add(food)
 repo.add(drink)
 print(repo.products)

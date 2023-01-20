0dff1ea6c4c72d5cd1e9b7850b63dcbdf3cd8d99
Atanas809 <atanas2380@abv.bg>
2023-01-20 08:35:06 +0200

    Added new file

---
+++ Python OOP/Inheritance - Exercise/exercises/main.py
@@ -0,0 +1,13 @@
+from drink import Drink
+from food import Food
+from product_repository import ProductRepository
+
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)

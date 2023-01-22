30021f999fac041eb54c9d7d22e31619a78c6446
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-22 11:42:54 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/exercises/main.py
+++ Python OOP/Inheritance - Exercise/exercises/main.py
@@ -4,3 +4,6 @@ from product import Product
 class ProductRepository:
     def __init__(self):
         self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)

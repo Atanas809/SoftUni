32daeb57ee2c1484b21d8d89eba3cc6b6acf5b22
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-22 11:43:52 +0200

    Update main.py

--- Python OOP/Inheritance - Exercise/exercises/main.py
+++ Python OOP/Inheritance - Exercise/exercises/main.py
@@ -15,3 +15,5 @@ class ProductRepository:
 
     def remove(self, product_name):
         for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)

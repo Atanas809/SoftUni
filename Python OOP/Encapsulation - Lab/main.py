e47a9b0b20ac5d29bc01eeb8950d9e801f087f4d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-23 08:09:36 +0200

    Update main.py

--- Python OOP/Encapsulation - Lab/main.py
+++ Python OOP/Encapsulation - Lab/main.py
@@ -14,3 +14,11 @@ class Account:
             return "Wrong pin"
         self.__pin = new_pin
         return "Pin changed"
+
+
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))

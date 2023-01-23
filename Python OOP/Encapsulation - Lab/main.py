74c24ea615c319a8945ea252d259af4b116700b8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-23 08:09:16 +0200

    Update main.py

--- Python OOP/Encapsulation - Lab/main.py
+++ Python OOP/Encapsulation - Lab/main.py
@@ -8,3 +8,9 @@ class Account:
         if pin != self.__pin:
             return "Wrong pin"
         return self.__id
+
+    def change_pin(self, old_pin, new_pin):
+        if old_pin != self.__pin:
+            return "Wrong pin"
+        self.__pin = new_pin
+        return "Pin changed"

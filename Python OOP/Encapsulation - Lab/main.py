cd98b72f5459d3f732a957dda31e0f6eccec51cb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-24 21:59:52 +0200

    Update main.py

--- Python OOP/Encapsulation - Lab/main.py
+++ Python OOP/Encapsulation - Lab/main.py
@@ -24,3 +24,8 @@ class EmailValidator:
 
 mails = ["gmail", "softuni"]
 domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))

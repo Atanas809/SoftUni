89f6921767cb9da458732ef51a363fd4a875c5b0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-27 08:31:57 +0200

    Update main.py

--- Python OOP/Encapsulation - Lab/main.py
+++ Python OOP/Encapsulation - Lab/main.py
@@ -2,3 +2,8 @@ class Profile:
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+
+    def is_valid(self, is_valid_length, containing_upper_letter, containing_digit):
+        if is_valid_length and containing_upper_letter and containing_digit:
+            return True
+        return False

0305830a989ea0a5fd5ac4608f2786f0028515f5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-27 08:33:08 +0200

    Update main.py

--- Python OOP/Encapsulation - Lab/main.py
+++ Python OOP/Encapsulation - Lab/main.py
@@ -31,3 +31,6 @@ class Profile:
         if not self.is_valid(is_valid_length, containing_upper_letter, containing_digit):
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
         self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'

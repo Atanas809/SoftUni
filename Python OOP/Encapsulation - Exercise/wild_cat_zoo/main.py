ed996f23621f134a03f117880e0f864f906ca21e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-28 10:28:26 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -4,3 +4,6 @@ class Animal:
         self.gender = gender
         self.age = age
         self.money_for_care = money_for_care
+
+    def __repr__(self):
+        return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"

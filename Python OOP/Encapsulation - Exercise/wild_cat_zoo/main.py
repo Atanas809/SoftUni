a05429a599bfa903c59025fb946a54e718cb54e8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-28 10:26:18 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,13 +1,4 @@
-from wild_cat_zoo.caretaker import Caretaker
-from wild_cat_zoo.cheetah import Cheetah
-from wild_cat_zoo.keeper import Keeper
-from wild_cat_zoo.lion import Lion
-from wild_cat_zoo.tiger import Tiger
-from wild_cat_zoo.vet import Vet
-from wild_cat_zoo.zoo import Zoo
-
-
-zoo = Zoo("Zootopia", 3000, 5, 8)
+5, 8)
 
 # Animals creation
 animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]

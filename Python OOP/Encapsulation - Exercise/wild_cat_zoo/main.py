134f165ca528929e2bc9f3fdcf57bc09cbaa0475
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-28 10:26:45 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,18 +1,1 @@
 
-
-# Adding all workers
-for worker in workers:
-    print(zoo.hire_worker(worker))
-
-# Tending animals
-print(zoo.tend_animals())
-
-# Paying keepers
-print(zoo.pay_workers())
-
-# Fireing worker
-print(zoo.fire_worker("Adam"))
-
-# Printing statuses
-print(zoo.animals_status())
-print(zoo.workers_status())

03612fd555988b18db89a34578703d87736dadc9
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-28 10:28:10 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,2 +1,6 @@
 class Animal:
     def __init__(self, name, gender, age, money_for_care):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.money_for_care = money_for_care

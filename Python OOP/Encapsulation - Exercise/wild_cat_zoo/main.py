39fe3a0e8d59735e448d5fb10b56bcf819df6f3a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-29 18:00:11 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,20 +1,5 @@
 
 
-# Animals creation
-animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
-
-# Animal prices
-prices = [200, 190, 204, 156, 211, 140]
-
-# Workers creation
-workers = [Keeper("John", 26, 100), Keeper("Adam", 29, 80), Keeper("Anna", 31, 95), Caretaker("Bill", 21, 68), Caretaker("Marie", 32, 105), Caretaker("Stacy", 35, 140), Vet("Peter", 40, 300), Vet("Kasey", 37, 280), Vet("Sam", 29, 220)]
-
-# Adding all animals
-for i in range(len(animals)):
-    animal = animals[i]
-    price = prices[i]
-    print(zoo.add_animal(animal, price))
-
 # Adding all workers
 for worker in workers:
     print(zoo.hire_worker(worker))

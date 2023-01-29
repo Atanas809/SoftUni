c493fcc6760ed471de3fd03c2bf1624f404790ed
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-29 18:01:37 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -3,3 +3,6 @@ class Worker:
         self.name = name
         self.age = age
         self.salary = salary
+
+    def __repr__(self):
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}"

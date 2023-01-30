9f4f3e53f801196c5d6a3f6c12238fc31f5892cb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-01-30 23:35:35 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,19 +1,4 @@
 
-    animal = animals[i]
-    price = prices[i]
-    print(zoo.add_animal(animal, price))
-
-# Adding all workers
-for worker in workers:
-    print(zoo.hire_worker(worker))
-
-# Tending animals
-print(zoo.tend_animals())
-
-# Paying keepers
-print(zoo.pay_workers())
-
-# Fireing worker
 print(zoo.fire_worker("Adam"))
 
 # Printing statuses

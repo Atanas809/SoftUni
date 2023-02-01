9558eaa8275e69eeeef2abf807679db608f83030
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-01 20:50:42 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,12 +1,4 @@
 
-
-
-zoo = Zoo("Zootopia", 3000, 5, 8)
-
-# Animals creation
-animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
-
-# Animal prices
 prices = [200, 190, 204, 156, 211, 140]
 
 # Workers creation

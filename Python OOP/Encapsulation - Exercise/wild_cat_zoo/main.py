4ecea04faf0ee4d909d00dc7b589576232506f98
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-02 23:13:50 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,10 +1,4 @@
-from wild_cat_zoo.caretaker import Caretaker
-from wild_cat_zoo.cheetah import Cheetah
-from wild_cat_zoo.keeper import Keeper
-from wild_cat_zoo.lion import Lion
-from wild_cat_zoo.tiger import Tiger
-from wild_cat_zoo.vet import Vet
-from wild_cat_zoo.zoo import Zoo
+
 
 
 zoo = Zoo("Zootopia", 3000, 5, 8)

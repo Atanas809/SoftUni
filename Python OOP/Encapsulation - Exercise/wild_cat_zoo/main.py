bffb26ace0f2f8b746cafe778287f4899358c25a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-02 23:14:18 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,9 +1,4 @@
 
-workers = [Keeper("John", 26, 100), Keeper("Adam", 29, 80), Keeper("Anna", 31, 95), Caretaker("Bill", 21, 68), Caretaker("Marie", 32, 105), Caretaker("Stacy", 35, 140), Vet("Peter", 40, 300), Vet("Kasey", 37, 280), Vet("Sam", 29, 220)]
-
-# Adding all animals
-for i in range(len(animals)):
-    animal = animals[i]
     price = prices[i]
     print(zoo.add_animal(animal, price))
 

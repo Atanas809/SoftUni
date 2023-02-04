bffa3865a0063f715d1f0d7099079126b582893e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-04 12:29:12 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -9,3 +9,10 @@ from wild_cat_zoo.worker import Worker
 
 
 class Zoo:
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals = []
+        self.workers = []

5b7a02ec4868986815c0fbbeb7b0961ad8cf4fbe
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-04 12:28:51 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -1,1 +1,8 @@
-
+from wild_cat_zoo.animal import Animal
+from wild_cat_zoo.caretaker import Caretaker
+from wild_cat_zoo.cheetah import Cheetah
+from wild_cat_zoo.keeper import Keeper
+from wild_cat_zoo.lion import Lion
+from wild_cat_zoo.tiger import Tiger
+from wild_cat_zoo.vet import Vet
+from wild_cat_zoo.worker import Worker

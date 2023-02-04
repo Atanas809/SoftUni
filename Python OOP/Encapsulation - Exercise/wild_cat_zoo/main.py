063ce093834b52750f042578db610f3c6340da92
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-04 12:30:04 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
+++ Python OOP/Encapsulation - Exercise/wild_cat_zoo/main.py
@@ -30,3 +30,8 @@ class Zoo:
     def hire_worker(self, worker: Worker):
         if len(self.workers) == self.__workers_capacity:
             return "Not enough space for worker"
+
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+
+    def fire_worker(self, worker_name):

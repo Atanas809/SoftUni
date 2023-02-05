1c313033fabf6a6ee0000a92fb7d64df605d150b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-05 13:32:48 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -27,3 +27,9 @@ class Dough:
     @property
     def weight(self):
         return self.__weight
+
+    @weight.setter
+    def weight(self, value):
+        if value <= 0:
+            raise ValueError("The weight cannot be less or equal to zero")
+        self.__weight = value

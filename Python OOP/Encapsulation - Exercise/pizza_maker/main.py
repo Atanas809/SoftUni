a453d660652b259e6ab2c8a507270da0c426d6ef
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-05 13:29:14 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -1,16 +1,4 @@
 
-    print(white_flour_dough.weight)
-    print(white_flour_dough.baking_technique)
-
-    whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
-    print(whole_wheat_dough.weight)
-    print(whole_wheat_dough.flour_type)
-    print(whole_wheat_dough.baking_technique)
-
-    p = Pizza("Margherita", whole_wheat_dough, 2)
-
-    p.add_topping(tomato_topping)
-    print(p.calculate_total_weight())
 
     p.add_topping(mozzarella_topping)
     print(p.calculate_total_weight())

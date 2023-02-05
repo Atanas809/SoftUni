f39046e120e992f5890f981c9e71513f47bff1e0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-05 13:31:09 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -3,3 +3,7 @@ class Dough:
         self.flour_type = flour_type
         self.baking_technique = baking_technique
         self.weight = weight
+
+    @property
+    def flour_type(self):
+        return self.__flour_type

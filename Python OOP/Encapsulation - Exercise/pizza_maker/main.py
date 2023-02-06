9cbf8ea8ac013f78b70f971cb09974e4095c76d3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-06 08:38:39 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -2,3 +2,7 @@ class Topping:
     def __init__(self, topping_type, weight):
         self.topping_type = topping_type
         self.weight = weight
+
+    @property
+    def topping_type(self):
+        return self.__topping_type

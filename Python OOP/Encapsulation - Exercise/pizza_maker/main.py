f14c6589bf9f21156f030d956538dd7a661d8e97
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-06 08:39:02 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -9,3 +9,6 @@ class Topping:
 
     @topping_type.setter
     def topping_type(self, value):
+        if not value:
+            raise ValueError("The topping type cannot be an empty string")
+        self.__topping_type = value

8263ccf66ec57682d803d81d6a23a51703810ac1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-07 08:47:37 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -1,13 +1,1 @@
 
-
-    p = Pizza("Margherita", whole_wheat_dough, 2)
-
-    p.add_topping(tomato_topping)
-    print(p.calculate_total_weight())
-
-    p.add_topping(mozzarella_topping)
-    print(p.calculate_total_weight())
-
-    p.add_topping(mozzarella_topping)
-except ValueError as error:
-    print(f'ValueError: {error}')

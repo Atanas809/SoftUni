511962ed01d53907a1e787a8ef8498fce2d09e57
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-07 08:49:10 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/pizza_maker/main.py
+++ Python OOP/Encapsulation - Exercise/pizza_maker/main.py
@@ -27,7 +27,24 @@ class Pizza:
         if value is None:
             raise ValueError("You should add dough to the pizza")
         self.__dough = value
-    
+
     @property
     def toppings_capacity(self):
         return self.__toppings_capacity
+
+    @toppings_capacity.setter
+    def toppings_capacity(self, value):
+        if value <= 0:
+            raise ValueError("The topping's capacity cannot be less or equal to zero")
+        self.__toppings_capacity = value
+
+    def add_topping(self, topping: Topping):
+        if self.toppings_capacity == len(self.toppings):
+            raise ValueError("Not enough space for another topping")
+
+        topping_type = topping.topping_type
+        weight = topping.weight
+
+        if topping_type not in self.toppings.keys():
+            self.toppings[topping_type] = 0
+        self.toppings[topping_type] += weight

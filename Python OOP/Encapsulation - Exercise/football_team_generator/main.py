79de5428da766aacdc1247c57627ff4e6b4c9408
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-08 23:06:56 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/football_team_generator/main.py
+++ Python OOP/Encapsulation - Exercise/football_team_generator/main.py
@@ -1,2 +1,7 @@
 class Player:
     def __init__(self, name, sprint, dribble, passing, shooting):
+        self.__name = name
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting

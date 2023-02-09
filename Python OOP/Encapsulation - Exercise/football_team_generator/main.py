de3e5f9fe1f3c301c95a918bf9b016126c0b995e
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-09 22:09:51 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/football_team_generator/main.py
+++ Python OOP/Encapsulation - Exercise/football_team_generator/main.py
@@ -2,3 +2,7 @@ from football_team_generator.player import Player
 
 
 class Team:
+    def __init__(self, name, rating):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []

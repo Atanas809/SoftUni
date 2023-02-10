359a1bcd8fa4487914362cf9c1bc6a272f828c1f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-10 22:28:04 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/football_team_generator/main.py
+++ Python OOP/Encapsulation - Exercise/football_team_generator/main.py
@@ -5,3 +5,7 @@ from football_team_generator.team import Team
 p = Player("Pall", 1, 3, 5, 7)
 
 print("Player name:", p.name)
+print("Points sprint:", p._Player__sprint)
+print("Points dribble:", p._Player__dribble)
+print("Points passing:", p._Player__passing)
+print("Points shooting:", p._Player__shooting)

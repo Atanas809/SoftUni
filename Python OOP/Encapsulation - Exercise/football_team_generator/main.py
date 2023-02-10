751c00bcfdc1a810787e7876f5639e9e00b3b2b8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-10 22:27:05 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/football_team_generator/main.py
+++ Python OOP/Encapsulation - Exercise/football_team_generator/main.py
@@ -1,8 +1,4 @@
 
-beverage = Beverage("coffee", 2.5, 50)
-print(beverage.__class__.__name__)
-print(beverage.__class__.__bases__[0].__name__)
-print(beverage.name)
 print(beverage.price)
 print(beverage.milliliters)
 soup = Soup("fish soup", 9.90, 230)

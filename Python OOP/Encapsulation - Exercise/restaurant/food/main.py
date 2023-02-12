e253ce5a2c44bf3d82a157428e7a674b605e2957
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-12 23:33:26 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/food/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/food/main.py
@@ -1,9 +1,3 @@
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

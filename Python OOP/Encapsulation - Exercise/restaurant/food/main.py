fe3ddb8dbd2fd731d8fc2a0e76f0404c6fd31dbc
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-13 08:38:12 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/food/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/food/main.py
@@ -5,3 +5,7 @@ class Dessert(Food):
     def __init__(self, name, price, grams, calories):
         super().__init__(name, price, grams)
         self.__calories = calories
+
+    @property
+    def calories(self):
+        return self.__calories

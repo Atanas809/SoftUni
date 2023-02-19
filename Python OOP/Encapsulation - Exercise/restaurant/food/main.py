b5b702903233a44abe00bf4f32516678d0e5affc
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-19 23:57:34 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/food/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/food/main.py
@@ -1,15 +1,5 @@
 
 
-    whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
-    print(whole_wheat_dough.weight)
-    print(whole_wheat_dough.flour_type)
-    print(whole_wheat_dough.baking_technique)
-
-    p = Pizza("Margherita", whole_wheat_dough, 2)
-
-    p.add_topping(tomato_topping)
-    print(p.calculate_total_weight())
-
     p.add_topping(mozzarella_topping)
     print(p.calculate_total_weight())
 

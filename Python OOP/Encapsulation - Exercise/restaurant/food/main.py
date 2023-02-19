7bbba535cbd8821e56ae6c9e881fdbc4b4ef69a5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-19 23:57:43 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/food/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/food/main.py
@@ -1,8 +1,1 @@
 
-
-    p.add_topping(mozzarella_topping)
-    print(p.calculate_total_weight())
-
-    p.add_topping(mozzarella_topping)
-except ValueError as error:
-    print(f'ValueError: {error}')

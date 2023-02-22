92f917005b78210f72125deeedf3bdc41a64094c
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-22 08:54:27 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
@@ -3,3 +3,5 @@ from restaurant.product import Product
 
 class Beverage(Product):
     def __init__(self, name, price, milliliters):
+        super().__init__(name, price)
+        self.__milliliters = milliliters

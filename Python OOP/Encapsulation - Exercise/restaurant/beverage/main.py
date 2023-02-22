198447132449bb0d4fecb092c16e5c5fd8db9dfb
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-22 08:54:39 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
@@ -5,3 +5,7 @@ class Beverage(Product):
     def __init__(self, name, price, milliliters):
         super().__init__(name, price)
         self.__milliliters = milliliters
+
+    @property
+    def milliliters(self):
+        return self.__milliliters

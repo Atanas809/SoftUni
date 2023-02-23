a720f5e6a39a2bcdbb38b257b8e2d988f8f298fe
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-23 21:55:48 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
@@ -3,3 +3,4 @@ from restaurant.beverage.beverage import Beverage
 
 class HotBeverage(Beverage):
     def __init__(self, name, price, milliliters):
+        super().__init__(name, price, milliliters)

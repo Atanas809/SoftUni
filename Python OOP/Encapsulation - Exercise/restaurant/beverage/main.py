bcfe6ad656f892fa0c918c21ed15d997c7378500
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-24 23:24:26 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/beverage/main.py
@@ -1,9 +1,1 @@
 
-print(beverage.price)
-print(beverage.milliliters)
-soup = Soup("fish soup", 9.90, 230)
-print(soup.__class__.__name__)
-print(soup.__class__.__bases__[0].__name__)
-print(soup.name)
-print(soup.price)
-print(soup.grams)

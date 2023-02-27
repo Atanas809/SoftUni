8e51fa410efa7a6fab02135018e3ea5e75ad94d7
Atanas809 <atanas2380@abv.bg>
2023-02-27 23:59:12 +0200

    Improve code for the new file

--- Python OOP/Encapsulation - Exercise/restaurant/main.py
+++ Python OOP/Encapsulation - Exercise/restaurant/main.py
@@ -1,3 +1,20 @@
-my_dict = {1: "1", 2: "2"}
+from restaurant.beverage.beverage import Beverage
+from restaurant.food.soup import Soup
+from restaurant.product import Product
 
-print(list(my_dict.items())[0])
+product = Product("coffee", 2.5)
+print(product.__class__.__name__)
+print(product.name)
+print(product.price)
+beverage = Beverage("coffee", 2.5, 50)
+print(beverage.__class__.__name__)
+print(beverage.__class__.__bases__[0].__name__)
+print(beverage.name)
+print(beverage.price)
+print(beverage.milliliters)
+soup = Soup("fish soup", 9.90, 230)
+print(soup.__class__.__name__)
+print(soup.__class__.__bases__[0].__name__)
+print(soup.name)
+print(soup.price)
+print(soup.grams)

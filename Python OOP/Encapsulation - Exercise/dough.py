c3984819f4493ab03bbf819e2237d3bc6ae5ef42
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:37:02 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -5,3 +5,12 @@ from wild_cat_zoo.lion import Lion
 from wild_cat_zoo.tiger import Tiger
 from wild_cat_zoo.vet import Vet
 from wild_cat_zoo.zoo import Zoo
+
+
+zoo = Zoo("Zootopia", 3000, 5, 8)
+
+# Animals creation
+animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
+
+# Animal prices
+prices = [200, 190, 204, 156, 211, 140]

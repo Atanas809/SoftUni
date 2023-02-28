ee72dde71db5a9c1c112901b54b9f9d8fb4500a8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:35:01 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -1,8 +1,4 @@
-class Dough:
-    def __init__(self, flour_type, baking_technique, weight):
-        self.flour_type = flour_type
-        self.baking_technique = baking_technique
-        self.weight = weight
+
 
     @property
     def flour_type(self):

2dd105a47c1f6b2aca40bc4049fa6cd3584826f7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:35:34 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -1,16 +1,6 @@
 
 
     @property
-    def baking_technique(self):
-        return self.__baking_technique
-
-    @baking_technique.setter
-    def baking_technique(self, value):
-        if not value:
-            raise ValueError("The baking technique cannot be an empty string")
-        self.__baking_technique = value
-
-    @property
     def weight(self):
         return self.__weight
 

c91236b33df57306134996cbd831baa348f5a8ec
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:36:45 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -1,1 +1,7 @@
-
+from wild_cat_zoo.caretaker import Caretaker
+from wild_cat_zoo.cheetah import Cheetah
+from wild_cat_zoo.keeper import Keeper
+from wild_cat_zoo.lion import Lion
+from wild_cat_zoo.tiger import Tiger
+from wild_cat_zoo.vet import Vet
+from wild_cat_zoo.zoo import Zoo

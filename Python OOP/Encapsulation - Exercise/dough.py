c77a389e58793353d143b59df866ceb53b7d4380
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:35:44 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -1,10 +1,4 @@
 
-
-    @property
-    def weight(self):
-        return self.__weight
-
-    @weight.setter
     def weight(self, value):
         if value <= 0:
             raise ValueError("The weight cannot be less or equal to zero")

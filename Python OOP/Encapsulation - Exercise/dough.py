b36ee8670dbca6d5d91b1000a47286504aa0cc87
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-02-28 23:38:03 +0200

    Update dough.py

--- Python OOP/Encapsulation - Exercise/dough.py
+++ Python OOP/Encapsulation - Exercise/dough.py
@@ -30,3 +30,13 @@ for worker in workers:
 
 # Tending animals
 print(zoo.tend_animals())
+
+# Paying keepers
+print(zoo.pay_workers())
+
+# Fireing worker
+print(zoo.fire_worker("Adam"))
+
+# Printing statuses
+print(zoo.animals_status())
+print(zoo.workers_status())

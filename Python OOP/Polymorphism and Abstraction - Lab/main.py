5d4765da7a473f70442d0d10c18660e5635923e0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-01 23:43:50 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Lab/main.py
+++ Python OOP/Polymorphism and Abstraction - Lab/main.py
@@ -2,3 +2,9 @@ class ImageArea:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    def get_area(self):
+        return self.height * self.width
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()

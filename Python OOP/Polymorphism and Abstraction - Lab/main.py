2bc88031ef2100314b677dbe9da00d5df765c266
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-01 23:44:10 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Lab/main.py
+++ Python OOP/Polymorphism and Abstraction - Lab/main.py
@@ -14,3 +14,10 @@ class ImageArea:
 
     def __eq__(self, other):
         return self.get_area() == other.get_area()
+
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 == a2)
+print(a1 != a3)

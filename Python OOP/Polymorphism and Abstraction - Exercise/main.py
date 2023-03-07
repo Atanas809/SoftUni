e4e85580faad972d5af9353fab0672bb0833387b
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-07 23:56:26 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/main.py
@@ -12,3 +12,10 @@ class Account:
         if not isinstance(amount, int):
             raise ValueError("please use int for amount")
         self._transactions.append(amount)
+
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        if account.balance + amount_to_add < 0:
+            raise ValueError("sorry cannot go in debt!")
+        account._transactions.append(amount_to_add)
+        return f"New balance: {account.balance}"

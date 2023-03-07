29f40ff0c745f6551de671e2ea40ba6d62416bd4
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-07 23:56:11 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/main.py
@@ -3,3 +3,12 @@ class Account:
         self.owner = owner
         self.amount = amount
         self._transactions = []
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        self._transactions.append(amount)

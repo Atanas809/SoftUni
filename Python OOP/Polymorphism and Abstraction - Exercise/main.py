1cb2f1a151278f276616bdb0ed55835726c26ca9
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-09 23:15:25 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/main.py
@@ -41,3 +41,10 @@ p4 = p2 + p3
 first_group = Group('__VIP__', [p0, p1, p2])
 second_group = Group('Special', [p3, p4])
 third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)

52ed1211f8e8c9f4f97b2ed656459474f3e9efd5
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-09 23:13:56 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/main.py
@@ -2,3 +2,6 @@ class Person:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"

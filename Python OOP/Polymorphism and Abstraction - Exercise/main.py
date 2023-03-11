b23da67d2f5f73204b74eba7ac04b5c96af66405
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-11 11:00:59 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/main.py
@@ -3,3 +3,8 @@ from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
     ADDITIONAL_LITERS = 0
+
+    @abstractmethod
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption

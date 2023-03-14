3087931fb3ff82726b35e977c0e05e0d7bce11e1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-14 08:53:54 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/project/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/project/main.py
@@ -2,3 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
+    @abstractmethod
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender

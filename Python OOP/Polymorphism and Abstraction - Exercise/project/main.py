883946376db25862ec9b0a505adea847096c5038
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-16 22:30:23 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/project/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/project/main.py
@@ -4,3 +4,6 @@ from project.animal import Animal
 class Cat(Animal):
     def __init__(self, name, age, gender):
         super().__init__(name, age, gender)
+
+    def make_sound(self):
+        return "Meow meow!"

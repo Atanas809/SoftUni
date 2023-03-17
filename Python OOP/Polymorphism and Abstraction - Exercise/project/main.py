6b0c8697596373ecc67b960c110f46f54d7e3a8f
Atanas809 <atanas2380@abv.bg>
2023-03-17 21:02:50 +0200

    Added new file

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/project/main.py
@@ -0,0 +1,9 @@
+from project.dog import Dog
+from project.tomcat import Tomcat
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)

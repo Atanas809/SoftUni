6c1e879517c9c22477952d592a054f2ad98ad4a7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-23 19:53:30 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -3,3 +3,8 @@ from abc import ABC, abstractmethod
 
 class Animal(ABC):
     INCREASE_WEIGHT = 0
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+        self.food_eaten = 0

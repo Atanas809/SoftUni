b01429f526f790d9e50fb1cd407c7ad3f6cda777
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-23 19:54:13 +0200

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -16,3 +16,11 @@ class Animal(ABC):
     @abstractmethod
     def make_sound(self):
         pass
+
+    def feed(self, food):
+        food_name = food.__class__.__name__
+
+        if food_name not in self.eats():
+            return f"{self.__class__.__name__} does not eat {food_name}!"
+        self.weight += food.quantity * self.INCREASE_WEIGHT
+        self.food_eaten += food.quantity

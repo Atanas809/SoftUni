639a1b98b799dc58dbf77a94283836602eee5780
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-27 22:53:41 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -1,12 +1,1 @@
 
-print("-" * 20)
-hen = Hen("Harry", 10, 10)
-veg = Vegetable(3)
-fruit = Fruit(5)
-meat = Meat(1)
-print(hen)
-print(hen.make_sound())
-hen.feed(veg)
-hen.feed(fruit)
-hen.feed(meat)
-print(hen)

950b88d68623f5338c1518b18da8f1aa68297cf7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-27 22:54:27 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -6,3 +6,9 @@ class Owl(Bird):
 
     def __init__(self, name, weight, wing_size):
         super().__init__(name, weight, wing_size)
+
+    def eats(self):
+        return ["Meat"]
+
+    def make_sound(self):
+        return "Hoot Hoot"

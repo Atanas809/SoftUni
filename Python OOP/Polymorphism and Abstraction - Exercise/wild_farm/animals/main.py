9d405d8a5fc60649a71cf5fffc5d7a42d0044c27
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-27 22:54:18 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -3,3 +3,6 @@ from wild_farm.animals.animal import Bird
 
 class Owl(Bird):
     INCREASE_WEIGHT = 0.25
+
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)

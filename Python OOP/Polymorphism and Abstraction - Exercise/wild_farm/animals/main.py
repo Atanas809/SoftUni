18f49d0d5fa3f9fdcef1f6842fa8b7230e9c1271
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-31 22:25:47 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -12,3 +12,10 @@ class Mouse(Mammal):
 
     def make_sound(self):
         return "Squeak"
+
+
+class Dog(Mammal):
+    INCREASE_WEIGHT = 0.40
+
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)

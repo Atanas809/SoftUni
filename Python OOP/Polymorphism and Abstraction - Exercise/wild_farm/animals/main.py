19cdd27a0b722cdc8179e39915403831f5c7f8f8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-31 22:26:07 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -25,3 +25,16 @@ class Dog(Mammal):
 
     def make_sound(self):
         return "Woof!"
+
+
+class Cat(Mammal):
+    INCREASE_WEIGHT = 0.30
+
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    def eats(self):
+        return ["Vegetable", "Meat"]
+
+    def make_sound(self):
+        return "Meow"

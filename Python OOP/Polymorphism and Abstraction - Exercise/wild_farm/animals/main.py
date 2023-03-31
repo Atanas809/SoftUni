0a6554715df707d1051a3e9d5b9e6b4cfd476141
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-03-31 22:25:26 +0300

    Update main.py

--- Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/animals/main.py
@@ -3,3 +3,6 @@ from wild_farm.animals.animal import Mammal
 
 class Mouse(Mammal):
     INCREASE_WEIGHT = 0.10
+
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)

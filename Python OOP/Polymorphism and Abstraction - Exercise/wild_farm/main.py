73405b9d182bb51feace36dcc8a21ceb0a0a82c7
Atanas809 <atanas2380@abv.bg>
2023-04-01 10:09:31 +0300

    Added new files for polymorphism

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/wild_farm/main.py
@@ -0,0 +1,22 @@
+from wild_farm.animals.birds import Owl, Hen
+from wild_farm.food import Meat, Vegetable, Fruit
+
+owl = Owl("Pip", 10, 10)
+print(owl)
+meat = Meat(4)
+print(owl.make_sound())
+owl.feed(meat)
+veg = Vegetable(1)
+print(owl.feed(veg))
+print(owl)
+print("-" * 20)
+hen = Hen("Harry", 10, 10)
+veg = Vegetable(3)
+fruit = Fruit(5)
+meat = Meat(1)
+print(hen)
+print(hen.make_sound())
+hen.feed(veg)
+hen.feed(fruit)
+hen.feed(meat)
+print(hen)

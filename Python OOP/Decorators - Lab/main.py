a81693374a04ec905c57c3dddff15cda54ccb5f0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-04-12 22:54:56 +0300

    Update main.py

--- Python OOP/Decorators - Lab/main.py
+++ Python OOP/Decorators - Lab/main.py
@@ -1,10 +1,4 @@
-def uppercase_decorator(function):
-    def wrapper():
-        func = function()
-        make_uppercase = func.upper()
-        return make_uppercase
 
-    return wrapper
 
 
 def split_string(function):

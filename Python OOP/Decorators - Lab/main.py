5120ccf2c6f8a435fb5da6b5df4a10a76b5001d0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-04-20 21:35:14 +0300

    Update main.py

--- Python OOP/Decorators - Lab/main.py
+++ Python OOP/Decorators - Lab/main.py
@@ -1,1 +1,9 @@
+def number_increment(numbers):
+    def increase():
+        result = []
+        for x in numbers:
+            result.append(x + 1)
 
+        return result
+
+    return increase()

c4e2bc5891cc917e82fb52fae58ad3c329b4e1d7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-02 23:00:36 +0300

    Update main.py

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -3,3 +3,10 @@ def make_bold(func):
         return f"<b>{func(*args)}</b>"
 
     return wrapper
+
+
+def make_italic(func):
+    def wrapper(*args):
+        return f"<i>{func(*args)}</i>"
+
+    return wrapper

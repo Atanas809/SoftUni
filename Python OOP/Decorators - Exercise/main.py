503e4eda35a28eeb483143a3ddfa04cf574de491
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-04 22:40:44 +0300

    Update main.py

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -4,3 +4,9 @@ def cache(func):
 
     def wrapper(number):
         wrapper.log = my_log
+
+        if number not in my_log:
+            my_log[number] = func(number)
+        return my_log[number]
+
+    return wrapper

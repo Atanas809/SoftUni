2b7c66e857317fb048c17e5ff9acba6045ee24b0
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-10 20:27:04 +0300

    Update main.py

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -9,3 +9,11 @@ def even_parameters(func):
         return result
 
     return wrapper
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result

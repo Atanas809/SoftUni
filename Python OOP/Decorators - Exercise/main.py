2d00cdd1b6b283e7b7b50030f229881c01a6bd9a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-10 20:26:55 +0300

    Update main.py

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -1,1 +1,11 @@
+def even_parameters(func):
+    def wrapper(*args):
+        for x in args:
+            if not isinstance(x, int) or x % 2 != 0:
+                return "Please use only even numbers!"
 
+        result = func(*args)
+
+        return result
+
+    return wrapper

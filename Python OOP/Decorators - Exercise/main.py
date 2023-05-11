29ef5e9b7d332cadca04eef506ef9209be6d28ff
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-11 21:44:10 +0300

    Update main.py

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -2,3 +2,11 @@ from time import time
 
 
 def exec_time(func):
+    def wrapper(*args):
+        start = time()
+        func(*args)
+        end = time()
+        execution_time = end - start
+        return execution_time
+
+    return wrapper

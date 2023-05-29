8f9faad15ae6cea512541cf614a799b8c625f1af
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-29 22:03:45 +0300

    Update file

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -1,2 +1,9 @@
 def tags(tag):
     def decorator(func):
+        def wrapper(*args):
+            result = func(*args)
+            return f"<{tag}>{result}</{tag}>"
+
+        return wrapper
+
+    return decorator

326fe2d8ca4b5754687966eeed4122f4ee157785
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-30 22:06:18 +0300

    Update file

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -1,3 +1,7 @@
 def logged(func):
     def wrapper(*args):
         result = ""
+
+        func_result = func(*args)
+        result += f"you called {func.__name__}{args}\n"
+        result += f"it returned {func_result}"

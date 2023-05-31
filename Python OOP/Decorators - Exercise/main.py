4c6035a0e73ccce613503585229b52568f1afc95
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-31 08:42:32 +0300

    Update file

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -1,3 +1,11 @@
 def type_check(types):
     def decorator(func):
         def wrapper(data):
+            if not isinstance(data, types):
+                return "Bad Type"
+
+            return func(data)
+
+        return wrapper
+
+    return decorator

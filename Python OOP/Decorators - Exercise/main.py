bff1791e234a0dea1f37c0bca47a64791a024fd7
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-05-31 08:42:59 +0300

    Update file

--- Python OOP/Decorators - Exercise/main.py
+++ Python OOP/Decorators - Exercise/main.py
@@ -14,3 +14,12 @@ def type_check(types):
 @type_check(int)
 def times2(num):
     return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]

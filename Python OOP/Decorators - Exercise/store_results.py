52f8d4fde9f1ec845864c16644a544905deae39f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-07-08 11:11:54 +0300

    Update store_results.py

--- Python OOP/Decorators - Exercise/store_results.py
+++ Python OOP/Decorators - Exercise/store_results.py
@@ -3,3 +3,7 @@ def store_results(func):
 
     def wrapper(*args):
         result = func(*args)
+        with open(default_file, "a") as file:
+            data = f"Function '{func.__name__}' was called. Result: {result}"
+            file.write(data)
+            file.write("\n")

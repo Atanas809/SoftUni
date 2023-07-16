4e8f968329bfb5b62b0ed8340ee49d78dd49b58f
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-07-16 11:16:59 +0300

    Update dip.py

--- Python OOP/SOLID - Lab/dip.py
+++ Python OOP/SOLID - Lab/dip.py
@@ -11,3 +11,18 @@ class Book:
 class OtherFormatter:
     def format(self, book: Book):
         return book.content
+
+
+class Formatter:
+    def format(self, book: Book) -> str:
+        return book.content
+
+
+class Printer:
+    def __init__(self, formatter):
+        self.formatter = formatter
+
+    def get_book(self, book: Book):
+        formatted_book = self.formatter.format(book)
+        return formatted_book
+    

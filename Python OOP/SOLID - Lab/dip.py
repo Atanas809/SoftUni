98837114b50686935e4b1f482c6762fd6056103a
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-07-16 11:17:20 +0300

    Update dip.py

--- Python OOP/SOLID - Lab/dip.py
+++ Python OOP/SOLID - Lab/dip.py
@@ -25,4 +25,22 @@ class Printer:
     def get_book(self, book: Book):
         formatted_book = self.formatter.format(book)
         return formatted_book
-    
+
+
+book = Book("Content1")
+book1 = Book("Content2")
+book2 = Book("Content3")
+
+f1 = Formatter()
+f2 = OtherFormatter()
+
+p1 = Printer(f1)
+p2 = Printer(f2)
+
+print(p1.get_book(book))
+print(p1.get_book(book1))
+print(p1.get_book(book2))
+print('-' * 10)
+print(p2.get_book(book))
+print(p2.get_book(book1))
+print(p2.get_book(book2))

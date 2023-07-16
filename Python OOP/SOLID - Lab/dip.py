61d44f1a77469f22bc8029b2c950f6ac12ec7f27
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2023-07-16 11:16:45 +0300

    Update dip.py

--- Python OOP/SOLID - Lab/dip.py
+++ Python OOP/SOLID - Lab/dip.py
@@ -1,3 +1,13 @@
 """
 DIP stands for - Dependency inversion Principle
 """
+
+
+class Book:
+    def __init__(self, content: str):
+        self.content = content
+
+
+class OtherFormatter:
+    def format(self, book: Book):
+        return book.content
